eecd2f5877ba4c116e6410df1eb9bdcd1b09888d
Michael Xue <michael.xue@duke.edu>
2019-06-13 15:23:12 -0400

    edited split_data.py

--- multimodal_example/split_data.py
+++ multimodal_example/split_data.py
@@ -14,9 +14,12 @@ train_input = input[rand]
 train_target = target[rand]
 train_seq_length = seq_length[rand]
 
-test_input = np.array([element for element in input if element not in train_input])
-test_target = np.array([element for element in input if element not in train_target])
-test_seq_length = np.array([element for element in input if element not in train_seq_length])
+test_input = [element for element in input if element not in train_input]
+test_input = np.array(test_input)
+test_target = [element for element in input if element not in train_target]
+test_target = np.array(test_target)
+test_seq_length = [element for element in input if element not in train_seq_length]
+test_seq_length = np.array(test_seq_length)
 
 train_sample = {"input": train_input, "target": train_target, "seq_length": train_seq_length}
 test_sample = {"input": input, "target": target, "seq_length": seq_length}
