90482b27e320d6facd3f5a47afbca7e952e13183
Michael Xue <michael.xue@duke.edu>
2019-06-13 15:48:02 -0400

    edited split_data.py

--- multimodal_example/split_data.py
+++ multimodal_example/split_data.py
@@ -14,9 +14,14 @@ train_input = input[rand]
 train_target = target[rand]
 train_seq_length = seq_length[rand]
 
-test_input = np.setdiff1d(input, train_input)
-test_target = np.setdiff1d(target, train_target)
-test_seq_length = np.setdiff1d(seq_length, train_seq_length)
+for x, y, z in train_input, train_target, train_seq_length:
+    input.remove(x)
+    target.remove(y)
+    seq_length.remove(z)
+
+#test_input = np.setdiff1d(input, train_input)
+#test_target = np.setdiff1d(target, train_target)
+#est_seq_length = np.setdiff1d(seq_length, train_seq_length)
 
 train_sample = {"input": train_input, "target": train_target, "seq_length": train_seq_length}
 test_sample = {"input": input, "target": target, "seq_length": seq_length}
