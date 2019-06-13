4e875d69b187aeaed3fb13175223d4fb3afe3a33
Ryan Culhane <rpc21@duke.edu>
2019-06-13 13:16:05 -0400

    Wrote custom collate function

--- multimodal_example/process_audio_torch.py
+++ multimodal_example/process_audio_torch.py
@@ -2,11 +2,12 @@ import torch
 import pickle
 from torch.utils.data import Dataset, DataLoader
 
+
 class IEMOCAP(Dataset):
     def __init__(self):
-        pickle_in = open('/scratch/speech/IEMOCAP_dictionary_5.pkl','rb')
+        pickle_in = open('/scratch/speech/IEMOCAP_dictionary_5.pkl', 'rb')
         data = pickle.load(pickle_in)
-        self.seq_len = data["seq_length"]
+        self.seq_length = data["seq_length"]
         self.input = data["input"]
         self.target = data["target"]
 
@@ -15,12 +16,22 @@ class IEMOCAP(Dataset):
 
     def __getitem__(self, index):
         sample = {'input': torch.from_numpy(self.input[index]),
-                    'target': torch.from_numpy(self.target[index]),
-                    'seq_len': torch.from_numpy(self.seq_len[index])}
+                  'target': torch.from_numpy(self.target[index]),
+                  'seq_length': torch.from_numpy(self.seq_length[index])}
         return sample
 
+
+def my_collate(batch):
+    input = [item for item in batch['input']]
+    target = [item for item in batch['target']]
+    seq_length = [item for item in batch['seq_length']]
+    target = torch.LongTensor(target)
+    seq_length = torch.LongTensor(seq_length)
+    return [input, target, seq_length]
+
+
 dataset = IEMOCAP()
-train_loader = DataLoader(dataset=dataset, batch_size=128, shuffle=True, num_workers=0)
+train_loader = DataLoader(dataset=dataset, batch_size=128, shuffle=True, collate_fn=my_collate, num_workers=0)
 for i, sample in enumerate(train_loader):
-    if (i < 3):
+    if i < 3:
         print(sample)
