b6cf4b8893d03f147b528c32213f28f0312e47c7
Ryan Culhane <rpc21@research-tarokhlab-02.oit.duke.edu>
2019-06-14 10:50:49 -0400

    Running into error where some things are not converted to GPU for argument #3 index

--- src/speech/SE_audio_torch.py
+++ src/speech/SE_audio_torch.py
@@ -16,16 +16,17 @@ class GRUAudio(nn.Module):
         self.num_layers = num_layers
         self.dropout_rate = dropout_rate
         self.num_labels = num_labels
-#        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-        self.gru = nn.GRU(self.num_features, self.hidden_dim, self.num_layers, batch_first=True, dropout=self.dropout_rate)
-        self.classification = nn.Linear(self.hidden_dim, self.num_labels)
+        self.gru = nn.GRU(self.num_features, self.hidden_dim, self.num_layers, batch_first=True, dropout=self.dropout_rate).to(self.device)
+        self.classification = nn.Linear(self.hidden_dim, self.num_labels).to(self.device)
 
     def forward(self, input, target):
-        input = input #.to(self.device)
-        target = target #.to(self.device)
+        input = input.to(self.device)
+        target = target.to(self.device)
         hidden = torch.randn(2, 128, 200)
-        hidden = hidden #.to(self.device)
+        hidden = hidden.to(self.device)
+        pdb.set_trace()
         out, hn = self.gru(input, hidden)
         out, _ = pad_packed_sequence(out, batch_first=True)
         out = self.classification(out)
