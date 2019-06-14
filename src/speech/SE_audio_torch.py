00b7aedfbc60ff16fe50d67bc879486f181c8b56
Ryan Culhane <rpc21@research-tarokhlab-02.oit.duke.edu>
2019-06-14 14:29:30 -0400

    Fully functional on gpu, rewriting dataloader for optimization

--- src/speech/SE_audio_torch.py
+++ src/speech/SE_audio_torch.py
@@ -11,12 +11,12 @@ class GRUAudio(nn.Module):
 
     def __init__(self, num_features, hidden_dim, num_layers, dropout_rate, num_labels):
         super(GRUAudio, self).__init__()
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         self.num_features = num_features
         self.hidden_dim = hidden_dim
         self.num_layers = num_layers
         self.dropout_rate = dropout_rate
         self.num_labels = num_labels
-        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
         self.gru = nn.GRU(self.num_features, self.hidden_dim, self.num_layers, batch_first=True, dropout=self.dropout_rate).to(self.device)
         self.classification = nn.Linear(self.hidden_dim, self.num_labels).to(self.device)
@@ -26,9 +26,11 @@ class GRUAudio(nn.Module):
         target = target.to(self.device)
         hidden = torch.randn(2, 128, 200)
         hidden = hidden.to(self.device)
-        pdb.set_trace()
+#        pdb.set_trace()
         out, hn = self.gru(input, hidden)
+#        pdb.set_trace()
         out, _ = pad_packed_sequence(out, batch_first=True)
+#        pdb.set_trace()
         out = self.classification(out)
         loss = F.cross_entropy(out, target)
         return out, loss
