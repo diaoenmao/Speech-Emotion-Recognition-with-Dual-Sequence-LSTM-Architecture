73a68edf8d0ea55368e7ba74f6f8a60fe111b751
wjyandre <wangjianyou100044@gmail.com>
2019-06-17 18:38:36 -0400

    adding more nl(num_layers)) paramters in model path

--- src/speech/full_train_and_test.py
+++ src/speech/full_train_and_test.py
@@ -121,20 +121,21 @@ def test_model(args):
 
     correct = 0
     print(len(test_loader))
+    print(len(testing_data))
     for test_case, target, seq_length in test_loader:
         test_case = pad_sequence(sequences=test_case, batch_first=True)
         test_case = pack_padded_sequence(test_case, lengths=seq_length, batch_first=True, enforce_sorted=False)
         out, loss = model(test_case, target, False)
-        pdb.set_trace()
-        index = torch.argmax(out)
-        if target[0][index] == 1:
-            #       print("Success!!")
-            correct += 1
+        index = torch.argmax(out,dim=1)
+        target_index=torch.argmax(target,dim=1)
+        temp= target_index==index
+        correct+=sum(temp).item()
 
     accuracy = correct * 1.0 / len(testing_data)
     print("accuracy:", accuracy)
     with open(stats_path, 'w+') as f:
         f.write("Accuracy of the model is: {}".format(accuracy))
+        print(stats_path)
 
 
 if __name__ == '__main__':
