6cf0612793619341209b4eded9eb87898830e35e
Ryan Culhane <rpc21@research-tarokhlab-02.oit.duke.edu>
2019-06-17 11:08:39 -0400

    Fully working cody pipeline, using train_audio then test_audio to build a model and test it. Going to push into master

--- src/speech/test_audio.py
+++ src/speech/test_audio.py
@@ -7,9 +7,9 @@ from torch.utils.data import DataLoader
 from SE_audio_torch import GRUAudio
 from process_audio_torch import IEMOCAP, my_collate
 
-model = GRUAudio(num_features=39, hidden_dim=400, num_layers=2, dropout_rate=0.7, num_labels=5, batch_size=1)
+model = GRUAudio(num_features=39, hidden_dim=200, num_layers=2, dropout_rate=0.7, num_labels=5, batch_size=1)
 model = model.cuda()
-model.load_state_dict(torch.load('/scratch/speech/models/classification/classifier.torch.pt'))
+model.load_state_dict(torch.load('/scratch/speech/models/classification/classifier.pt'))
 model.eval()
 
 testing_data = IEMOCAP(train=False)
@@ -17,11 +17,14 @@ test_loader = DataLoader(dataset=testing_data, batch_size=1, shuffle=True, colla
 print("Loading successful")
 
 correct = 0
+print(len(test_loader))
 for i, (test_case, target, _) in enumerate(test_loader):
-    test_case=test_case[0]
+    if i % 100 == 0:
+        print(i)
+    test_case = test_case[0]
     test_case= test_case.reshape(1,test_case.shape[0],test_case.shape[1])
     out, loss = model(test_case,target,False)
-    pdb.set_trace()
+#    pdb.set_trace()
     index = torch.argmax(out)
     print(index)
   #  pdb.set_trace()
