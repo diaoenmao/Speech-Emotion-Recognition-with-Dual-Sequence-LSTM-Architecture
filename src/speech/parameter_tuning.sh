e4f9538623b071001884e5718680147cf14c84af
Ryan Culhane <rpc21@research-tarokhlab-02.oit.duke.edu>
2019-06-17 22:21:55 -0400

    Tweaked the testing method to be able to perform analysis and write to text file

--- src/speech/parameter_tuning.sh
+++ src/speech/parameter_tuning.sh
@@ -27,5 +27,5 @@ CUDA_VISIBLE_DEVICES="2" python full_train_and_test.py -dr 0.0 -b 256 -lr 0.001 
 CUDA_VISIBLE_DEVICES="0" python full_train_and_test.py -dr 0.0 -b 256 -lr 0.001 -e 50 -hd 50 --nl 6
 CUDA_VISIBLE_DEVICES="1" python full_train_and_test.py -dr 0.0 -b 256 -lr 0.001 -e 50 -hd 50 -nl 7
 CUDA_VISIBLE_DEVICES="2" python full_train_and_test.py -dr 0.0 -b 256 -lr 0.001 -e 50 -hd 50 -nl 8
-CUDA_VISIBLE_DEVICES="0" python full_train_and_test.py -dr 0.2 -b 256 -lr 0.001 -e 20 -bi -hd 250
-CUDA_VISIBLE_DEVICES="1" python full_train_and_test.py -dr 0.2 -b 256 -lr 0.001 -e 20 -bi -hd 250
+CUDA_VISIBLE_DEVICES="0" python full_train_and_test.py -dr 0.2 -b 256 -lr 0.001 -e 50 -bi -hd 250
+CUDA_VISIBLE_DEVICES="1" python full_train_and_test.py -dr 0.2 -b 256 -lr 0.001 -e 50 -bi -hd 250
