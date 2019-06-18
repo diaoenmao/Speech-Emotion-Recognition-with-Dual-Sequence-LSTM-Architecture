aa8f3c47465167641e50270d7612cca0fd2cb36a
Ryan Culhane <rpc21@research-tarokhlab-02.oit.duke.edu>
2019-06-18 14:47:14 -0400

    Feature extraction done for specific emotions

--- src/preprocessing/feature_extraction_emos.py
+++ src/preprocessing/feature_extraction_emos.py
@@ -8,7 +8,7 @@ import pdb
 
 OPENSMILE_CONFIG_PATH = '/scratch/speech/opensmile/opensmile-2.3.0/config/MFCC12_E_D_A.conf'
 DATASET_PATH = '/scratch/speech/datasets/'
-out_file = '/scratch/speech/Speech-Emotion-Analysis/src/preprocessing/temp.csv'
+out_file = '/scratch/speech/Speech-Emotion-Analysis/src/preprocessing/temp_emo.csv'
 
 in_file = '/scratch/speech/IEMOCAP_full_release/Session1/sentences/wav/Ses01F_impro01/Ses01F_impro01_F000.wav'
 
