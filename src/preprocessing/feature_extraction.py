ca038126399e6c76b2c34afed63bd89e43088920
Ryan Culhane <rpc21@duke.edu>
2019-06-18 12:12:45 -0400

    Cleaned up feature extraction to be more flexible for extracting different features

--- src/preprocessing/feature_extraction.py
+++ src/preprocessing/feature_extraction.py
@@ -1,11 +1,88 @@
 import os
+import argparse
+import pandas as pd
 import numpy as np
+import pickle
+from sklearn.model_selection import train_test_split
+import pdb
 
 OPENSMILE_CONFIG_PATH = '/scratch/speech/opensmile/opensmile-2.3.0/config/MFCC12_E_D_A.conf'
+DATASET_PATH = '/scratch/speech/datasets/'
 out_file = '/scratch/speech/Speech-Emotion-Analysis/src/preprocessing/temp.csv'
 
 in_file = '/scratch/speech/IEMOCAP_full_release/Session1/sentences/wav/Ses01F_impro01/Ses01F_impro01_F000.wav'
+
+
+def init_parser():
+    parser = argparse.ArgumentParser(
+        description='Use this script to extract features using OpenSMILE by specifying the path to the configuration file')
+    parser.add_argument('-config_filename', '-cf', default=OPENSMILE_CONFIG_PATH, help='Path to the configuration file',
+                        dest='config_path')
+    parser.add_argument('-dataset_name', '-dn', help='Specify the dataset name / output file (pkl)',
+                        dest='dataset_name')
+    parser.add_argument('-time_series', '-ts', default=False, action='store_true',
+                        help='Include if extracts time series data', dest='time_series')
+    return parser.parse_args()
+
+
+def load_paths_and_labels():
+    return pd.read_csv('audio_paths_labels.csv')
+
+
+def extract_features(args, dataframe):
+    input = []
+    target = []
+    for file, emotion, valence, activation, dominance in dataframe:
+        cmd = 'SMILExtract -C {} -I {} -csvoutput {} -headercsv 0'.format(args.config_path, file, out_file)
+        os.system(cmd)
+        df = pd.read_csv(out_file)
+        input.append(df.values)
+        target.append(emotion)
+    input = np.array(input)
+    target = np.array(target)
+    return input, target
+
+
+def extract_features_ts(args, dataframe):
+    input, target = extract_features(args, dataframe)
+    seq_length = np.array([x.shape[0] for x in input])
+    return input, target, seq_length
+
+
+def split_data(data, time_series):
+    if time_series:
+        input_train, input_test, target_train, target_test, seq_length_train, seq_length_test = train_test_split(
+            data["input"], data["target"], data["seq_length"], test_size=0.2, random_state=42)
+        train = {'input': input_train, 'target': target_train, 'seq_length': seq_length_train}
+        test = {'input': input_test, 'target': target_test, 'seq_length': seq_length_test}
+        return train, test
+    else:
+        input_train, input_test, target_train, target_test = train_test_split(
+            data["input"], data["target"], test_size=0.2, random_state=42)
+        train = {'input': input_train, 'target': target_train}
+        test = {'input': input_test, 'target': target_test}
+        return train, test
+
+
+def save(args, dataset):
+    train, test = split_data(dataset, args.time_series)
+    with open(DATASET_PATH + args.dataset_name + '_full.pkl', 'wb') as f:
+        pickle.dump(dataset, f)
+    with open(DATASET_PATH + args.dataset_name + '_train.pkl', 'wb') as f:
+        pickle.dump(train, f)
+    with open(DATASET_PATH + args.dataset_name + 'test.pkl', 'wb') as f:
+        pickle.dump(test, f)
+
+
 if __name__ == '__main__':
-	cmd = 'SMILExtract -C ' + OPENSMILE_CONFIG_PATH + ' -I ' + in_file + ' -csvoutput ' + out_file + ' -headercsv 0'
-	os.system(cmd)
- 
+    args = init_parser()
+    pdb.set_trace()
+    dataframe = load_paths_and_labels()
+    pdb.set_trace()
+    if args.time_series:
+        input, target, seq_length = extract_features_ts(args, dataframe)
+        dataset = {'input': input, 'target': target, 'seq_length': seq_length}
+    else:
+        input, target = extract_features(args, dataframe)
+        dataset = {'input': input, 'target': target}
+    save(args, dataset)
--- src/preprocessing/label_extraction.py
+++ src/preprocessing/label_extraction.py
@@ -11,17 +11,18 @@ def print_list(list_to_print):
 		print(item)
 
 
-if __name__ == '__main__':
+def process_text_files():
 	audio_paths = []
 	categorical_emotion = []
 	average_valence = []
 	average_activation = []
 	average_dominance = []
 	for x in range(5):
-		session_path = PATH_TO_DATA + 'Session' + str(x+1)
+		session_path = PATH_TO_DATA + 'Session' + str(x + 1)
 		label_path = session_path + '/dialog/EmoEvaluation'
 		audio_path = session_path + '/sentences/wav/'
-		files = [label_path + '/' + f for f in os.listdir(label_path) if os.path.isfile(os.path.join(label_path,f)) and f[0] != '.']
+		files = [label_path + '/' + f for f in os.listdir(label_path) if
+				 os.path.isfile(os.path.join(label_path, f)) and f[0] != '.']
 		# print_list(files)
 		for file in files:
 			lines = []
@@ -40,12 +41,12 @@ if __name__ == '__main__':
 				average_valence.append(float(scores[0]))
 				average_activation.append(float(scores[1]))
 				average_dominance.append(float(scores[2]))
-	# for i in range(10):
-	# 	print(audio_paths[i], categorical_emotion[i], average_valence[i], average_activation[i], average_dominance[i])
-	# 	f = wave.open(audio_paths[i],'r')
-	# 	print(f.getframerate())
-	# 	f.close()
-	# print(len(audio_paths), len(categorical_emotion), len(average_valence), len(average_activation), len(average_dominance))
-	df = pd.DataFrame(list(zip(audio_paths, categorical_emotion, average_valence, average_activation, average_dominance)), columns=['file', 'emotion', 'valence', 'activation', 'dominance'])
+	return pd.DataFrame(
+		list(zip(audio_paths, categorical_emotion, average_valence, average_activation, average_dominance)),
+		columns=['file', 'emotion', 'valence', 'activation', 'dominance'])
+
+
+if __name__ == '__main__':
+	df = process_text_files()
 	df.to_csv('/scratch/rpc21/Speech-Emotion-Analysis/src/preprocessing/audio_paths_labels.csv',index=False)
 	# print('wrote to csv')
