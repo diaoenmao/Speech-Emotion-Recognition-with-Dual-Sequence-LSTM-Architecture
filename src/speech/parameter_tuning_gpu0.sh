02794aae851e7555153e6d4275891100ed532fed
Ryan Culhane <rpc21@research-tarokhlab-02.oit.duke.edu>
2019-06-19 15:03:04 -0400

    Adjust lstm_audio so that it runs with full_train_and_test

--- src/speech/parameter_tuning_gpu0.sh
+++ src/speech/parameter_tuning_gpu0.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-CUDA_VISIBLE_DEVICES="0" python full_train_and_test.py -m att_gru -dr 0.0 -b 256 -lr 0.001 -e 50 -hd 200 -tc -no_train
-CUDA_VISIBLE_DEVICES="0" python full_train_and_test.py -m att_gru -dr 0.0 -b 256 -lr 0.0001 -e 50 -hd 200 -tc -no_train
-CUDA_VISIBLE_DEVICES="0" python full_train_and_test.py -m att_gru -dr 0.2 -b 256 -lr 0.0001 -e 50 -hd 200 -tc -no_train
-CUDA_VISIBLE_DEVICES="0" python full_train_and_test.py -m att_gru -dr 0.4 -b 256 -lr 0.0001 -e 50 -hd 200 -tc -no_train
-CUDA_VISIBLE_DEVICES="0" python full_train_and_test.py -m att_gru -dr 0.6 -b 256 -lr 0.0001 -e 50 -hd 200 -tc -no_train
-CUDA_VISIBLE_DEVICES="0" python full_train_and_test.py -m att_gru -dr 0.8 -b 256 -lr 0.0001 -e 50 -hd 200 -tc -no_train
+CUDA_VISIBLE_DEVICES="0" python full_train_and_test.py -m lstm -dr 0.0 -b 256 -lr 0.001 -e 50 -hd 200 -bi -tc
+CUDA_VISIBLE_DEVICES="0" python full_train_and_test.py -m lstm -dr 0.0 -b 256 -lr 0.001 -e 50 -hd 200 -tc
+CUDA_VISIBLE_DEVICES="0" python full_train_and_test.py -m lstm -dr 0.2 -b 256 -lr 0.001 -e 50 -hd 200 -tc
+CUDA_VISIBLE_DEVICES="0" python full_train_and_test.py -m lstm -dr 0.4 -b 256 -lr 0.001 -e 50 -hd 200 -tc
+CUDA_VISIBLE_DEVICES="0" python full_train_and_test.py -m lstm -dr 0.3 -b 256 -lr 0.001 -e 50 -hd 200 -tc
+CUDA_VISIBLE_DEVICES="0" python full_train_and_test.py -m lstm -dr 0.0 -b 256 -lr 0.001 -e 50 -hd 250 -tc
