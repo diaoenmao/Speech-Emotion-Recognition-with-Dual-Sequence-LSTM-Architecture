0f7c1901900e7fe938f65d81b40681a0646c97f0
Ryan Culhane <rpc21@research-tarokhlab-02.oit.duke.edu>
2019-06-19 10:51:23 -0400

    Feature extraction appears to be working correctly for 39 features, prosodic still has bugs and is only giving 4 features in time series

--- src/preprocessing/feature_extraction.py
+++ src/preprocessing/feature_extraction.py
@@ -35,7 +35,10 @@ def extract_features(args, dataframe):
     for file, emotion, valence, activation, dominance in dataframe.values:
         cmd = 'SMILExtract -C {} -I {} -csvoutput {} -headercsv 0'.format(args.config_path, file, out_file)
         os.system(cmd)
-        df = pd.read_csv(out_file, delimiter=';').iloc[:,1:]
+        if args.time_series:
+            df = pd.read_csv(out_file, delimiter=';').iloc[:,2:]
+        else:
+            df = pd.read_csv(out_file, delimiter=';').iloc[:,1:]
         input.append(df.values)
         target.append(emotion)
 #    input = np.array(input)
@@ -76,7 +79,7 @@ def save(args, dataset):
 
 if __name__ == '__main__':
     args = init_parser()
-    dataframe = load_paths_and_labels()
+    dataframe = load_paths_and_labels().head()
     if args.time_series:
         input, target, seq_length = extract_features_ts(args, dataframe)
         dataset = {'input': input, 'target': target, 'seq_length': seq_length}
