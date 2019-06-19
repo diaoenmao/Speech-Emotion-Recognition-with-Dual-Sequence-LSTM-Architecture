d2a7f89c8b944f2914f20a0c26ce72969d880e66
Ryan Culhane <rpc21@research-tarokhlab-02.oit.duke.edu>
2019-06-19 12:42:07 -0400

    Preparing to run training scripts for attention and mean pooling

--- src/speech/parameter_tuning_gpu3.sh
+++ src/speech/parameter_tuning_gpu3.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-CUDA_VISIBLE_DEVICES="3" python full_train_and_test.py -dr 0.8 -b 256 -lr 0.001 -e 50 -bi -hd 200 -tc -no_train -no_test
-CUDA_VISIBLE_DEVICES="3" python full_train_and_test.py -dr 0.8 -b 256 -lr 0.001 -e 50 -hd 200 -tc -no_train -no_test
-CUDA_VISIBLE_DEVICES="3" python full_train_and_test.py -dr 0.8 -b 256 -lr 0.001 -e 50 -hd 200 -tc -no_train -no_test
+CUDA_VISIBLE_DEVICES="3" python full_train_and_test.py -m mean_pool_gru -dr 0.0 -b 256 -lr 0.001 -e 50 -hd 200 -tc
+CUDA_VISIBLE_DEVICES="3" python full_train_and_test.py -m mean_pool_gru -dr 0.0 -b 256 -lr 0.0001 -e 50 -hd 200 -tc
+CUDA_VISIBLE_DEVICES="3" python full_train_and_test.py -m mean_pool_gru -dr 0.2 -b 256 -lr 0.0001 -e 50 -hd 200 -tc
+CUDA_VISIBLE_DEVICES="3" python full_train_and_test.py -m mean_pool_gru -dr 0.4 -b 256 -lr 0.0001 -e 50 -hd 200 -tc
+CUDA_VISIBLE_DEVICES="3" python full_train_and_test.py -m mean_pool_gru -dr 0.6 -b 256 -lr 0.0001 -e 50 -hd 200 -tc
+CUDA_VISIBLE_DEVICES="3" python full_train_and_test.py -m mean_pool_gru -dr 0.8 -b 256 -lr 0.0001 -e 50 -hd 200 -tc
