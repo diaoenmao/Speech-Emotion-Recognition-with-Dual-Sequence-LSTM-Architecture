a144e53b848645083a92891fd6ff3c583f8f765c
Ryan Culhane <rpc21@research-tarokhlab-02.oit.duke.edu>
2019-06-19 14:38:18 -0400

    Fixed bugs in how arguments are passed to allow for testing

--- src/speech/full_train_and_test.py
+++ src/speech/full_train_and_test.py
@@ -117,7 +117,7 @@ def test_model(args, model_path, stats_path, checkpoint):
     for test_case, target, seq_length in test_loader:
         test_case = pad_sequence(sequences=test_case, batch_first=True)
         test_case = pack_padded_sequence(test_case, lengths=seq_length, batch_first=True, enforce_sorted=False)
-        out, loss = model(test_case, target, False, seq_length=seq_length)
+        out, loss = model(test_case, target, train=False, seq_length=seq_length)
         index = torch.argmax(out, dim=1)
         target_index = torch.argmax(target, dim=1).to(device)
         losses += loss.item() * index.shape[0]
