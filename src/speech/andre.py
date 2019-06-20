d2281331e06e781b9f654cfc64deab9b45d34ad2
wjyandre <wangjianyou100044@gmail.com>
2019-06-20 12:18:52 -0400

    changes

--- src/speech/andre.py
+++ src/speech/andre.py
@@ -67,3 +67,48 @@ class ATT(nn.Module):
         loss = F.cross_entropy(out_2_normalized, torch.max(target, 1)[1])
 #        print(self.u[10])
         return out_final, loss
+class Mean_Pool_2(nn.module):
+    def __init__(self, num_features, hidden_dim, num_layers, dropout_rate, num_labels, batch_size, bidirectional=False):
+        super(ATT, self).__init__()
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.num_features = num_features
+        self.hidden_dim = hidden_dim
+        self.num_layers = num_layers
+        self.dropout_rate = dropout_rate
+        self.num_labels = num_labels
+        self.batch_size = batch_size
+        self.bidirectional = bidirectional
+        self.num_directions = 1 + self.bidirectional
+        #self.attn = nn.Linear(self.hidden_dim * self.num_directions, hidden_dim)
+        #self.u=nn.Parameter(torch.randn(self.hidden_dim))
+        #stdv = 1. / math.sqrt(self.u.shape[0])
+        #self.u.data.normal_(mean=0, std=stdv)
+        self.lstm = nn.LSTM(self.num_features, self.hidden_dim, self.num_layers, batch_first=True, dropout=self.dropout_rate, bidirectional=self.bidirectional).to(self.device)
+        self.fc1 = nn.Linear(self.hidden_dim * self.num_directions, self.hidden_dim).to(self.device)
+        self.batch1=nn.BatchNorm1d(self.hidden_dim)
+        self.fc2=nn.Linear(self.hidden_dim,self.num_labels).to(self.device)
+
+        self.batch2=nn.BatchNorm1d(self.num_labels)
+        self.batchatt=nn.BatchNorm1d(self.hidden_dim * self.num_directions)
+
+    def forward(self, input, target, seq_length, train=True):
+        input = input.to(self.device)
+        target = target.to(self.device)
+
+        out, hn = self.lstm(input)
+
+        out , _ =pad_packed_sequence(out,batch_first=True)
+
+        x=torch.mean(out,dim=1)
+
+        input_linear_normalized=self.batchatt(x)
+        out_1 = self.fc1(input_linear_normalized)
+        out_1_normalized=self.batch1(out1)
+        out_2=self.fc2(out_1_normalized)
+        out_2_normalized=self.batch2(out2)
+
+        
+        loss = F.cross_entropy(out_2_normalized, torch.max(target, 1)[1])
+#        print(self.u[10])
+        return out_final, loss
+
