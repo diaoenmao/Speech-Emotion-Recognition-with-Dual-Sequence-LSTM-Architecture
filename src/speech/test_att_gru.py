e1bbfd98e707d6fbbbdd2a8b6f2ba7fdbc35631e
Jianyou Wang <jw542@research-tarokhlab-02.oit.duke.edu>
2019-06-20 10:13:16 -0400

    change

--- src/speech/test_att_gru.py
+++ src/speech/test_att_gru.py
@@ -10,8 +10,8 @@ from process_audio_torch import IEMOCAP, my_collate
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
-model = ATT(num_features=39, hidden_dim=250, num_layers=2, dropout_rate=0,
-                     num_labels=4, batch_size=256,bidirectional=True)
+model = ATT(num_features=39, hidden_dim=300, num_layers=2, dropout_rate=0.2,
+                     num_labels=4, batch_size=128,bidirectional=True)
 model = model.cuda()
 model.load_state_dict(torch.load("/scratch/speech/models/classification/deep_att_classifier_epoch_10.pt"))
 model.eval()
