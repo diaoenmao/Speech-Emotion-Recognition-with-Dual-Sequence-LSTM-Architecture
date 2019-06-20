a080138deca0bfe3b731405ba1bd7616857d3a25
Jianyou Wang <jw542@research-tarokhlab-02.oit.duke.edu>
2019-06-20 17:02:49 -0400

    running code for tonight

--- src/speech/parameter_tuning_gpu0.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-CUDA_VISIBLE_DEVICES="0" python full_train_and_test.py -m lstm -dr 0.0 -b 256 -lr 0.001 -e 50 -hd 200 -bi -tc
-CUDA_VISIBLE_DEVICES="0" python full_train_and_test.py -m lstm -dr 0.0 -b 256 -lr 0.001 -e 50 -hd 200 -tc
-CUDA_VISIBLE_DEVICES="0" python full_train_and_test.py -m lstm -dr 0.2 -b 256 -lr 0.001 -e 50 -hd 200 -tc
-CUDA_VISIBLE_DEVICES="0" python full_train_and_test.py -m lstm -dr 0.4 -b 256 -lr 0.001 -e 50 -hd 200 -tc
-CUDA_VISIBLE_DEVICES="0" python full_train_and_test.py -m lstm -dr 0.3 -b 256 -lr 0.001 -e 50 -hd 200 -tc
-CUDA_VISIBLE_DEVICES="0" python full_train_and_test.py -m lstm -dr 0.0 -b 256 -lr 0.001 -e 50 -hd 250 -tc
