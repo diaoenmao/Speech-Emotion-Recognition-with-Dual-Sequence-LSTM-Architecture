1c2872764145ac26d43079f6528221903a9c781b
Michael Xue <michael.xue@duke.edu>
2019-06-21 15:33:05 -0400

    changes

--- src/preprocessing/audio_paths_parse.py
+++ src/preprocessing/audio_paths_parse.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-in_file = '/scratch/speech/datasets/audio_paths_labels.csv'
+in_file = '/scratch/speech/datasets/audio_paths_labels_1.csv'
 out_file = '/scratch/speech/raw_audio_dataset/audio_paths_labels_updated.csv'
 
 df = pd.read_csv(in_file)
