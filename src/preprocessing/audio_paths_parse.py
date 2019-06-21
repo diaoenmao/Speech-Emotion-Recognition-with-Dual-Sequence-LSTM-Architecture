3129ca28e105c286b049cac06ca007fd53b85156
Michael Xue <michael.xue@duke.edu>
2019-06-21 14:15:51 -0400

    created audio_paths_parse.py

---
+++ src/preprocessing/audio_paths_parse.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+in_file = './audio_paths_labels.csv'
+out_file = './audio_paths_labels_updated.csv'
+
+df = pd.read_csv(in_file)
+
+emotions = ['hap', 'exc', 'sad', 'ang', 'neu']
+audio_paths = []
+categorical_emotion = []
+for file, emotion, valence, activation, dominance in df.values:
+    if emotion in emotions:
+        audio_paths.append(file)
+        categorical_emotion.append(emotion)
+
+df_updated = pd.DataFrame(list(zip(audio_paths, categorical_emotion), columns=['file', 'emotion'])
+df_updated.to_csv(out_file, index=False)
