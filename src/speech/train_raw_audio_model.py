6bfb935c5d5f647cc5e8b0a3d721f0ff1297898e
Michael Xue <michael.xue@duke.edu>
2019-06-25 10:30:45 -0400

    changes

--- src/speech/train_raw_audio_model.py
+++ src/speech/train_raw_audio_model.py
@@ -42,7 +42,7 @@ for epoch in range(3):  # again, normally you would NOT do 300 epochs, it is toy
         model.zero_grad()
         out, loss = model(input, target, seq_length=seq_length)
         pdb.set_trace()
-        losses += loss.item() * target.shape[0]
+        losses += torch.mean(loss).item() * target.shape[0]
         loss.backward()
         optimizer.step()
 
