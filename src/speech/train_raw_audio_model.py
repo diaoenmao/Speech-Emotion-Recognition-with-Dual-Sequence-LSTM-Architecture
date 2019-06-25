dd85860931589f51b0aecb1c38278a4f60af7f75
Michael Xue <michael.xue@duke.edu>
2019-06-25 10:14:29 -0400

    changes

--- src/speech/train_raw_audio_model.py
+++ src/speech/train_raw_audio_model.py
@@ -5,11 +5,12 @@ from process_raw_audio_model import IEMOCAP, my_collate
 from torch.utils.data import DataLoader
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 import pdb
+from torch.nn import DataParallel
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-model = RawAudioModel(1, 64, 3, 1, 1, 4, 4, 200, 2, 0, 4, 8)
-model.cuda()
+model = RawAudioModel(1, 64, 3, 1, 1, 4, 4, 200, 2, 0, 4, 32)
+model=DataParallel(model,device_ids=[0,1,2,3])
 model.train()
 
 # Use Adam as the optimizer with learning rate 0.01 to make it fast for testing purposes
@@ -18,7 +19,7 @@ scheduler = ReduceLROnPlateau(optimizer=optimizer,factor=0.3, patience=8, thresh
 
 # Load the training data
 training_data = IEMOCAP(train=True)
-train_loader = DataLoader(dataset=training_data, batch_size=8, shuffle=True, collate_fn=my_collate, num_workers=0)
+train_loader = DataLoader(dataset=training_data, batch_size=32, shuffle=True, collate_fn=my_collate, num_workers=0)
 testing_data = IEMOCAP(train=False)
 test_loader = DataLoader(dataset=testing_data, batch_size=32, shuffle=True, collate_fn=my_collate, num_workers=0)
 
@@ -35,6 +36,7 @@ for epoch in range(3):  # again, normally you would NOT do 300 epochs, it is toy
     correct_test = 0
     model.train()
     for j, (input, target, seq_length) in enumerate(train_loader):
+        print("================================= Batch"+ str(j)+ "===================================================")
         input = input.unsqueeze(1)
         model.zero_grad()
         out, loss = model(input, target, seq_length=seq_length)
@@ -45,9 +47,12 @@ for epoch in range(3):  # again, normally you would NOT do 300 epochs, it is toy
         index = torch.argmax(out, dim=1)
         target_index = torch.argmax(target, dim=1).to(device)
         correct += sum(index == target_index).item()
+        print("loss: ",loss)
 
     accuracy=correct*1.0/len(training_data)
     losses=losses / len(training_data)
+    print("accuracy: ", accuracy)
+    print("losses: ", losses)
 
     #after training
     model.eval()
