1a19006db400fb3cbe3a1a77e17bed93d8a1e3ba
Michael Xue <michael.xue@duke.edu>
2019-06-25 12:03:48 -0400

    changes

--- src/preprocessing/equalize_seq_length2.py
+++ src/preprocessing/equalize_seq_length2.py
@@ -18,24 +18,24 @@ seq_length_new = []
 
 for i, utterance in enumerate(data['input']):
     if len(utterance) < thresh:
-        utterance_new = np.tile(utterance, thresh // len(utterance) + 1)
-        utterance_new = utterance_new[0:thresh]
+        utterance_new = np.append(utterance, np.zeros(thresh - len(utterance)))
         input_new.append(utterance_new)
         seq_length_new.append(len(utterance_new))
+        print(len(utterance_new))
     elif len(utterance) > thresh:
-        utterance_new, sr = librosa.load(df['file'][i], sr = thresh/(len(utterance)/sr_standard))
-        utterance_new = utterance_new[0:thresh]
+        utterance_new = utterance[0:thresh]
         input_new.append(utterance_new)
         seq_length_new.append(len(utterance_new))
+        print(len(utterance_new))
     else:
         input_new.append(utterance)
         seq_length_new.append(len(utterance))
 
 dataset_updated = {'input': np.array(input_new), 'target': np.array(data['target']), 'seq_length': seq_length_new}
 
-full_set_new = '/scratch/speech/raw_audio_dataset/raw_audio_full_equal_lengths.pkl'
-train_set_new = '/scratch/speech/raw_audio_dataset/raw_audio_train_equal_lengths.pkl'
-test_set_new = '/scratch/speech/raw_audio_dataset/raw_audio_test_equal_lengths.pkl'
+full_set_new = '/scratch/speech/raw_audio_dataset/raw_audio_full_equal_lengths2.pkl'
+train_set_new = '/scratch/speech/raw_audio_dataset/raw_audio_train_equal_lengths2.pkl'
+test_set_new = '/scratch/speech/raw_audio_dataset/raw_audio_test_equal_lengths2.pkl'
 
 train, test = split_data(dataset_updated)
 with open(full_set_new, 'wb') as f:
