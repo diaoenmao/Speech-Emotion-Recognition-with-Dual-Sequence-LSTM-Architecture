2dd143119acfd641234fa456b68277e2e3b1e901
Michael Xue <michael.xue@duke.edu>
2019-06-25 10:44:50 -0400

    changes

--- src/speech/train_raw_audio_model.py
+++ src/speech/train_raw_audio_model.py
@@ -22,7 +22,7 @@ scheduler = ReduceLROnPlateau(optimizer=optimizer,factor=0.3, patience=8, thresh
 training_data = IEMOCAP(train=True)
 train_loader = DataLoader(dataset=training_data, batch_size=160, shuffle=True, collate_fn=my_collate, num_workers=0)
 testing_data = IEMOCAP(train=False)
-test_loader = DataLoader(dataset=testing_data, batch_size=32, shuffle=True, collate_fn=my_collate, num_workers=0)
+test_loader = DataLoader(dataset=testing_data, batch_size=160, shuffle=True, collate_fn=my_collate, num_workers=0)
 
 test_acc=[]
 train_acc=[]
@@ -54,6 +54,25 @@ for epoch in range(10):  # again, normally you would NOT do 300 epochs, it is to
     losses=losses / len(training_data)
     print("accuracy: ", accuracy)
     print("losses: ", losses)
+
+    model.eval()
+    for test_case, target, seq_length in test_loader:
+        out, loss = model(test_case, target, train=False, seq_length=seq_length)
+        index = torch.argmax(out, dim=1)
+        target_index = torch.argmax(target, dim=1).to(device)
+        loss = torch.mean(loss)
+        losses_test += loss.item() * index.shape[0]
+        correct_test += sum(index == target_index).item()
+    accuracy_test = correct_test * 1.0 / len(testing_data)
+    losses_test = losses_test / len(testing_data)
+
+    # data gathering
+    test_acc.append(accuracy_test)
+    train_acc.append(accuracy)
+    test_loss.append(losses_test)
+    train_loss.append(losses)
+
+    print("Training Loss: {} -------- Testing Loss: {} -------- Training Acc: {} -------- Testing Acc: {}".format(losses,losses_test, accuracy, accuracy_test))
 
 
     scheduler.step(losses)
