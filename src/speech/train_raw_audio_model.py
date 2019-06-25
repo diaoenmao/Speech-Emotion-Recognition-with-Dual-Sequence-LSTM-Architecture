5513ae70ae0907e31c9b93501d89ad4e2e76be23
Michael Xue <michael.xue@duke.edu>
2019-06-25 10:05:58 -0400

    changes

--- src/speech/train_raw_audio_model.py
+++ src/speech/train_raw_audio_model.py
@@ -8,7 +8,7 @@ import pdb
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-model = RawAudioModel(1, 64, 3, 1, 1, 4, 4, 200, 2, 0, 4, 64)
+model = RawAudioModel(1, 64, 3, 1, 1, 4, 4, 200, 2, 0, 4, 8)
 model.cuda()
 model.train()
 
@@ -18,7 +18,7 @@ scheduler = ReduceLROnPlateau(optimizer=optimizer,factor=0.3, patience=8, thresh
 
 # Load the training data
 training_data = IEMOCAP(train=True)
-train_loader = DataLoader(dataset=training_data, batch_size=64, shuffle=True, collate_fn=my_collate, num_workers=0)
+train_loader = DataLoader(dataset=training_data, batch_size=8, shuffle=True, collate_fn=my_collate, num_workers=0)
 testing_data = IEMOCAP(train=False)
 test_loader = DataLoader(dataset=testing_data, batch_size=32, shuffle=True, collate_fn=my_collate, num_workers=0)
 
