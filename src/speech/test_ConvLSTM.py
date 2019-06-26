a2ee1861d024df49b3205954f043d38ee388ca17
wjyandre <wangjianyou100044@gmail.com>
2019-06-26 17:21:59 -0400

    convolution LSTM

--- src/speech/test_ConvLSTM.py
+++ src/speech/test_ConvLSTM.py
@@ -11,11 +11,11 @@ from torch.nn import DataParallel
 
 
 model = ConvLSTM(1, [64,32,16],[9,5,5],100)
+model.cuda()
 model=DataParallel(model,device_ids=[0,1,2,3])
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 epoch=1
-model_dict=torch.load("/scratch/speech/models/classification/ConvLSTM_checkpoint_epoch_{}.pt".format(epoch))
-model=model.load_state_dict(model_dict)
+model=model.load_state_dict(torch.load("/scratch/speech/models/classification/ConvLSTM_checkpoint_epoch_{}.pt".format(epoch)))
 
 training_data = IEMOCAP(train=True)
 train_loader = DataLoader(dataset=training_data, batch_size=60, shuffle=True, collate_fn=my_collate, num_workers=0)
