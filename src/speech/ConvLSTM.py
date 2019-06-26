ea0547db3afa3e92585a33e0938571b293aae433
wjyandre <wangjianyou100044@gmail.com>
2019-06-26 14:50:40 -0400

    convolution LSTM

---
+++ src/speech/ConvLSTM.py
@@ -0,0 +1,111 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import pdb
+
+
+class ConvLSTMCell(nn.Module):
+    def __init__(self, input_channels, hidden_channels, kernel_size,kernel_size_pool=8, stride_pool=4):
+        super(ConvLSTMCell, self).__init__()
+
+        assert hidden_channels % 2 == 0
+
+        self.input_channels = input_channels
+        self.hidden_channels = hidden_channels
+        self.kernel_size = kernel_size
+        self.stride=1
+        self.padding = int((kernel_size - 1) / 2)
+        self.kernel_size_pool=kernel_size_pool
+        self.stride_pool=stride_pool
+
+        self.Wxi = nn.Conv1d(self.input_channels, self.hidden_channels, self.kernel_size, self.stride,self.padding,  bias=True)
+        self.Whi = nn.Conv1d(self.hidden_channels, self.hidden_channels, self.kernel_size, self.stride, self.padding, bias=False)
+
+        self.Wxf = nn.Conv1d(self.input_channels, self.hidden_channels, self.kernel_size, self.stride,self.padding,  bias=True)
+        self.Whf = nn.Conv1d(self.hidden_channels, self.hidden_channels, self.kernel_size, self.stride,self.padding, bias=False)
+
+        self.Wxc = nn.Conv1d(self.input_channels, self.hidden_channels, self.kernel_size, self.stride, self.padding, bias=True)
+        self.Whc = nn.Conv1d(self.hidden_channels, self.hidden_channels, self.kernel_size, self.stride, self.padding, bias=False)
+
+        self.Wxo = nn.Conv1d(self.input_channels, self.hidden_channels, self.kernel_size, self.stride,self.padding,  bias=True)
+        self.Who = nn.Conv1d(self.hidden_channels, self.hidden_channels, self.kernel_size, self.stride, self.padding, bias=False)
+        
+        self.max_pool = nn.MaxPool1d(self.kernel_size_pool, stride=self.stride_pool)
+        self.batch = nn.BatchNorm1d(self.hidden_channels)
+
+        self.Wci = None
+        self.Wcf = None
+        self.Wco = None
+
+    def forward(self, x, h, c):
+        assert c*self.Wci.size()==self.Whi(h).size(), "c*Wci size {}, Whi(h) size{}".format(c*self.Wci.size(),self.Whi(h).size())
+
+        ci = torch.sigmoid(self.Wxi(x) + self.Whi(h) + c * self.Wci)
+        cf = torch.sigmoid(self.Wxf(x) + self.Whf(h) + c * self.Wcf)
+        cc = cf * c + ci * torch.tanh(self.Wxc(x) + self.Whc(h))
+        co = torch.sigmoid(self.Wxo(x) + self.Who(h) + cc * self.Wco)
+        ch = co * torch.tanh(cc)
+        ch_pool=self.batch(self.max_pool(ch))
+        return ch_pool, ch, cc
+
+    def init_hidden(self, batch_size, hidden, shape):
+        if self.Wci is None:
+            self.Wci = nn.Parameters(torch.zeros(batch_size, hidden, shape)).cuda()
+            self.Wcf = nn.Parameters(torch.zeros(batch_size, hidden, shape)).cuda()
+            self.Wco = nn.Parameters(torch.zeros(batch_size, hidden, shape)).cuda()
+
+        return (nn.Parameters(torch.zeros(batch_size, hidden, shape)).cuda(),
+                nn.Parameters(torch.zeros(batch_size, hidden, shape)).cuda())
+
+
+class ConvLSTM(nn.Module):
+    # input_channels corresponds to the first input feature map
+    # hidden state is a list of succeeding lstm layers.
+    # kernel size is also a list, same length as hidden_channels
+    def __init__(self, input_channels, hidden_channels, kernel_size, step):
+        super(ConvLSTM, self).__init__()
+        assert len(hidden_channels)==len(kernel_size), "size mismatch"
+        self.input_channels = [input_channels] + hidden_channels
+        self.hidden_channels = hidden_channels
+        self.kernel_size = kernel_size
+        self.num_layers = len(hidden_channels)
+        self.step = step
+        self._all_layers = []
+        self.num_labels=4
+        self.classification = nn.Linear(100, self.num_labels)
+        for i in range(self.num_layers):
+            name = 'cell{}'.format(i)
+            cell = ConvLSTMCell(self.input_channels[i], self.hidden_channels[i], self.kernel_size)
+            setattr(self, name, cell)
+            self._all_layers.append(cell)
+
+    def forward(self, input, target, seq_length, train=True):
+        # input should be a list of inputs, like a time stamp, maybe 1280 for 100 times. 
+        internal_state = []
+        outputs = []
+        for step in range(self.step):
+            x = input[step]
+            for i in range(self.num_layers):
+                name = 'cell{}'.format(i)
+                if step == 0:
+                    bsize, _, shape = x.size()
+                    (h, c) = getattr(self, name).init_hidden(batch_size=bsize, hidden=self.hidden_channels[i],
+                                                             shape=shape)
+                    internal_state.append((h, c))
+
+                # do forward
+                (h, c) = internal_state[i]
+                x, new_h, new_c = getattr(self, name)(x, h, c)
+                internal_state[i] = (new_h, new_c)
+            outputs.append(x)
+        ## mean pooling and loss function
+        pdb.set_trace()
+        out = torch.mean(outputs, dim=1)
+
+        out = self.classification(out)
+
+        loss = F.cross_entropy(out, torch.max(target, 1)[1])
+
+
+        return out, loss
+
---
+++ src/speech/train_ConvLSTM.py
@@ -0,0 +1,89 @@
+import torch
+from torch import optim
+from raw_audio_model import RawAudioModel
+from ConvLSTM import ConvLSTM
+from process_raw_audio_model import IEMOCAP, my_collate
+from torch.utils.data import DataLoader
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+from torch.optim.lr_scheduler import CosineAnnealingLR
+import pdb
+from torch.nn import DataParallel
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+model = ConvLSTM(1, 64, [64,32,16],[8,4,4],100)
+model.cuda()
+model=DataParallel(model,device_ids=[0,1,2,3])
+model.train()
+
+# Use Adam as the optimizer with learning rate 0.01 to make it fast for testing purposes
+optimizer = optim.SGD(model.parameters(), lr=0.01)
+#scheduler = ReduceLROnPlateau(optimizer=optimizer,factor=0.3, patience=5, threshold=1e-3)
+scheduler =CosineAnnealingLR(optimizer, T_max=100, eta_min=0.0001)
+# Load the training data
+training_data = IEMOCAP(train=True)
+train_loader = DataLoader(dataset=training_data, batch_size=100, shuffle=True, collate_fn=my_collate, num_workers=0)
+testing_data = IEMOCAP(train=False)
+test_loader = DataLoader(dataset=testing_data, batch_size=100, shuffle=True, collate_fn=my_collate, num_workers=0)
+
+test_acc=[]
+train_acc=[]
+test_loss=[]
+train_loss=[]
+
+for epoch in range(10):  # again, normally you would NOT do 300 epochs, it is toy data
+    print("===================================" + str(epoch+1) + "==============================================")
+    losses = 0
+    correct=0
+    losses_test = 0
+    correct_test = 0
+    model.train()
+    for j, (input, target, seq_length) in enumerate(train_loader):
+       # if (j+1)%50==0: print("================================= Batch"+ str(j+1)+ "===================================================")
+        input=input.float()
+        input = input.unsqueeze(1)
+        input=torch.split(input,1280,dim=2)
+        model.zero_grad()
+        out, loss = model(input, target, seq_length=seq_length)
+        loss = torch.mean(loss)
+        losses += loss.item() * target.shape[0]
+        loss.backward()
+        optimizer.step()
+
+        index = torch.argmax(out, dim=1)
+        target_index = torch.argmax(target, dim=1).to(device)
+        correct += sum(index == target_index).item()
+
+    accuracy=correct*1.0/len(training_data)
+    losses=losses / len(training_data)
+
+    model.eval()
+    for test_case, target, seq_length in test_loader:
+        test_case=test_case.float()
+        test_case = test_case.unsqueeze(1)
+        test_case=torch.split(test_case,1280,dim=2)
+        out, loss = model(test_case, target, train=False, seq_length=seq_length)
+        index = torch.argmax(out, dim=1)
+        target_index = torch.argmax(target, dim=1).to(device)
+        loss = torch.mean(loss)
+        losses_test += loss.item() * index.shape[0]
+        correct_test += sum(index == target_index).item()
+    accuracy_test = correct_test * 1.0 / len(testing_data)
+    losses_test = losses_test / len(testing_data)
+
+    # data gathering
+    test_acc.append(accuracy_test)
+    train_acc.append(accuracy)
+    test_loss.append(losses_test)
+    train_loss.append(losses)
+    print("Epoch: {}-----------Training Loss: {} -------- Testing Loss: {} -------- Training Acc: {} -------- Testing Acc: {}".format(epoch+1,losses,losses_test, accuracy, accuracy_test)+"\n")
+    with open("/scratch/speech/models/classification/ConvLSTM_checkpoint_stats.txt","a+") as f:
+        f.write("Epoch: {}-----------Training Loss: {} -------- Testing Loss: {} -------- Training Acc: {} -------- Testing Acc: {}".format(epoch+1,losses,losses_test, accuracy, accuracy_test)+"\n")
+
+
+    scheduler.step()
+
+
+pickle_out=open("/scratch/speech/models/classification/ConvLSTM_checkpoint_stats.pkl","wb")
+pickle.dump({"test_acc":test_acc, "train_acc": train_acc, "test_loss": test_loss, "train_loss": train_loss},pickle_out)
+pickle_out.close()
