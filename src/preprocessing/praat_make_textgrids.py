99c639a74e627c637529baa968ea015c779bbcc6
Michael Xue <michael.xue@duke.edu>
2019-06-27 14:15:13 -0400

    changes

--- src/preprocessing/praat_make_textgrids.py
+++ src/preprocessing/praat_make_textgrids.py
@@ -13,9 +13,12 @@ endpoint = ''
 
 encode = {"hap": [1, 0, 0, 0], "exc": [1, 0, 0, 0], "neu": [0, 1, 0, 0], "ang": [0, 0, 1, 0], "sad": [0, 0, 0, 1]}
 
-def create_textgrids():
+save_path = '/scratch/speech/raw_audio_dataset/'
+
+def extract_features():
     input = []
     target = []
+
     for file, emotion in df.values:
         script_path = '/scratch/speech/modularProsodyTagger/mod01.praat'
         index = file.rfind('/')
@@ -23,7 +26,7 @@ def create_textgrids():
         directory = file[:60] + basename[:-5] + '/'
         endpoint = '/scratch/speech/textgrids/'
         cmd = 'praat --run {} {} {} {}'.format(script_path, directory, basename, endpoint)
-        os.system(cmd)
+        #os.system(cmd)
 
         tgrid_path = endpoint + basename + '_result.TextGrid'
         tgrid = textgrid.read_textgrid(tgrid_path)
@@ -31,15 +34,38 @@ def create_textgrids():
 
         data = []
         indices = []
-        sample_rate = 0
+
         for start, stop, name, tier in tgrid_df.values:
             sample_rate, data = wavfile.read(file)
             if tier != 'silences':
                 break
             else:
-                indices.append(stop)
+                indices.append(round(stop * sample_rate))
         data = data.tolist()
         data = [data[i : j] for i, j in zip([0] + indices, indices + [None])]
-
+        print(data)
         input.append(data)
         target.append(encode[emotion])
+
+        return input, target
+
+def split_data(data):
+    input_train, input_test, target_train, target_test = train_test_split(
+        data["input"], data["target"], test_size=0.2, random_state=42)
+    train = {'input': input_train, 'target': target_train}
+    test = {'input': input_test, 'target': target_test}
+    return train, test
+
+def save(dataset):
+    train, test = split_data(dataset)
+    with open(save_path + 'raw_audio_segmented' + '_full.pkl', 'wb') as f:
+        pickle.dump(dataset, f)
+    with open(save_path + 'raw_audio_segmented' + '_train.pkl', 'wb') as f:
+        pickle.dump(train, f)
+    with open(save_path + 'raw_audio_segmented' + '_test.pkl', 'wb') as f:
+        pickle.dump(test, f)
+
+if __name__ == '__main__':
+    input, target = extract_features(df)
+    dataset = {'input': input, 'target': target}
+    save(dataset)
