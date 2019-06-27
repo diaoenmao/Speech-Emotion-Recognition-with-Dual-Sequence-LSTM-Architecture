afd4e390b502445fce4e06d7deecdb9474dd43eb
Michael Xue <michael.xue@duke.edu>
2019-06-27 09:54:37 -0400

    creates textgrid file for each utterancein pratt_make_textgrids.py

--- src/preprocessing/praat_make_textmaps.py
+++ src/preprocessing/praat_make_textmaps.py
@@ -3,3 +3,12 @@ import pandas as pd
 
 in_file = '/scratch/speech/raw_audio_dataset/audio_paths_labels_updated.csv'
 df = pd.read_csv(in_file)
+
+for file, _ in df.values:
+    script_path = '/modularProsodyTagger/mod01.praat'
+    index = file.rfind('/')
+    basename = file[(index + 1):-4]
+    directory = file[:60] + basename[:-5] + '/'
+    endpoint = '/scratch/speech/textgrids/'
+    cmd = 'praat --run {} {} {} {}'.format(script_path, basename, directory, endpoint)
+    os.system(cmd)
