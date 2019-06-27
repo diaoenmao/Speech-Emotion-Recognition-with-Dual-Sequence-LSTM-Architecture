077bdfbacedc6fb82dc388ec306c9c01d92d3c33
Michael Xue <michael.xue@duke.edu>
2019-06-27 10:03:07 -0400

    changed script_path

--- src/preprocessing/praat_make_textgrids.py
+++ src/preprocessing/praat_make_textgrids.py
@@ -5,10 +5,10 @@ in_file = '/scratch/speech/raw_audio_dataset/audio_paths_labels_updated.csv'
 df = pd.read_csv(in_file)
 
 for file, _ in df.values:
-    script_path = '/modularProsodyTagger/mod01.praat'
+    script_path = '/scratch/speech/modularProsodyTagger/mod01.praat'
     index = file.rfind('/')
     basename = file[(index + 1):-4]
     directory = file[:60] + basename[:-5] + '/'
     endpoint = '/scratch/speech/textgrids/'
     cmd = 'praat --run {} {} {} {}'.format(script_path, basename, directory, endpoint)
-    os.system(cmd) 
+    os.system(cmd)
