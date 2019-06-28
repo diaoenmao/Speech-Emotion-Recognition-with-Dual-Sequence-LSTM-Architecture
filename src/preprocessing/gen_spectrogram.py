6d80a46fdad454c7d96fe6d43f8cbf80ce33cee5
Michael Xue <michael.xue@duke.edu>
2019-06-28 11:24:32 -0400

    changes

--- src/preprocessing/gen_spectrogram.py
+++ src/preprocessing/gen_spectrogram.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 from scipy import signal
 from scipy.io import wavfile
 import pandas as pd
+import numpy as np
 
 in_file = '/scratch/speech/raw_audio_dataset/audio_paths_labels_updated.csv'
 df = pd.read_csv(in_file)
@@ -9,8 +10,8 @@ df = pd.read_csv(in_file)
 sample_rate, samples = wavfile.read(df.values[0][0])
 frequencies, times, spectrogram = signal.spectrogram(samples, sample_rate)
 
-plt.pcolormesh(times, frequencies, spectrogram)
-plt.imshow(spectrogram)
+plt.pcolormesh(times, frequencies, np.log(spectrogram))
+#plt.imshow(spectrogram)
 plt.ylabel('Frequency [Hz]')
 plt.xlabel('Time [sec]')
 plt.show()
