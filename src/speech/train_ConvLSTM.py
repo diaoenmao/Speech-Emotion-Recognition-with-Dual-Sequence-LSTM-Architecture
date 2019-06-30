5d2fa21ed493696b2fbf20f6df59e8fe52918db3
wjyandre <wangjianyou100044@gmail.com>
2019-06-30 04:43:51 -0400

    changes

--- src/speech/train_ConvLSTM.py
+++ src/speech/train_ConvLSTM.py
@@ -23,10 +23,10 @@ model.train()
 
 # Use Adam as the optimizer with learning rate 0.01 to make it fast for testing purposes
 optimizer = optim.Adam(model.parameters(), lr=0.005)
-optimizer2=optim.SGD(model.parameters(), lr=0.01,momentum=0.9)
+optimizer2=optim.SGD(model.parameters(), lr=0.01)
 scheduler = ReduceLROnPlateau(optimizer=optimizer,factor=0.5, patience=2, threshold=1e-3)
-scheduler2=ReduceLROnPlateau(optimizer=optimizer2, factor=0.5, patience=2, threshold=1e-3)
-#scheduler =CosineAnnealingLR(optimizer, T_max=100, eta_min=0.0001)
+#scheduler2=ReduceLROnPlateau(optimizer=optimizer2, factor=0.5, patience=2, threshold=1e-3)
+scheduler2 =CosineAnnealingLR(optimizer2, T_max=100, eta_min=0.0001)
 # Load the training data
 training_data = IEMOCAP(train=True)
 train_loader = DataLoader(dataset=training_data, batch_size=120, shuffle=True, collate_fn=my_collate, num_workers=0)
@@ -39,7 +39,7 @@ test_loss=[]
 train_loss=[]
 epoch=0
 torch.save(model.state_dict(), "/scratch/speech/models/classification/ConvLSTM_checkpoint_epoch_{}.pt".format(epoch))
-for epoch in range(200):  # again, normally you would NOT do 300 epochs, it is toy data
+for epoch in range(100):  # again, normally you would NOT do 300 epochs, it is toy data
     print("===================================" + str(epoch+1) + "==============================================")
     losses = 0
     correct=0
@@ -100,7 +100,7 @@ for epoch in range(200):  # again, normally you would NOT do 300 epochs, it is t
             correct_test += sum(index == target_index).item()
     accuracy_test = correct_test * 1.0 / (len(testing_data)-res)
     losses_test = losses_test / (len(testing_data)-res)
-    if losses_test<1.0: scheduler=scheduler2
+    if losses_test<0.95: scheduler=scheduler2; optimizer=optimizer2
 
     # data gathering
     test_acc.append(accuracy_test)
