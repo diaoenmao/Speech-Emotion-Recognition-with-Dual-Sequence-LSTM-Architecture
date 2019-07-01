58d5fb2da580629cfc7b7047c9159c3dfbf58ea7
wjyandre <wangjianyou100044@gmail.com>
2019-07-01 16:00:19 -0400

    changes

--- src/speech/process_raw_audio_model.py
+++ src/speech/process_raw_audio_model.py
@@ -5,16 +5,25 @@ from torch.utils.data import Dataset
 import pdb
 
 class IEMOCAP(Dataset):
-    def __init__(self, train=True):
+    def __init__(self, train=True, mike=False):
+        self.mike=mike
         pickle_in = ''
-        if train:
+        if train and mike: 
+            pickle_in=open('/scratch/speech/raw_audio_dataset/raw_audio_separate_segments_train_equal_lengths.pkl',"rb")
+        elif train and not mike: 
             pickle_in = open('/scratch/speech/raw_audio_dataset/raw_audio_train_equal_lengths.pkl', 'rb')
             #pickle_in = open('/scratch/speech/IEMOCAP_dictionary_5_train.pkl','rb')
+        elif not train and mike:
+            pickle_in=open('/scratch/speech/raw_audio_dataset/raw_audio_separate_segments_test_equal_lengths.pkl',"rb")
         else:
             pickle_in = open('/scratch/speech/raw_audio_dataset/raw_audio_test_equal_lengths.pkl', 'rb')
             #pickle_in = open('/scratch/speech/IEMOCAP_dictionary_5_test.pkl','rb')
         data = pickle.load(pickle_in)
-        self.seq_length = data["seq_length"]
+        if not mike:
+            self.seq_length = data["seq_length"]
+        else:
+            self.seq_length=None
+
 #        pdb.set_trace()
         self.input = data["input"]
         self.target = data["target"]
@@ -33,7 +42,9 @@ def my_collate(batch):
     input = torch.from_numpy(np.array([item['input'] for item in batch])).to(device)
     # input = [x.cuda() for x in input]
     target = torch.from_numpy(np.array([item['target'] for item in batch]))
-    seq_length = torch.from_numpy(np.array([item['seq_length'] for item in batch]))
+    if not mike:
+        seq_length = torch.from_numpy(np.array([item['seq_length'] for item in batch]))
+    else: seq_length=None
     # seq_length = [x[0] for x in seq_length]
     # seq_length = torch.from_numpy(np.array(seq_length))
     return [input, target, seq_length]
