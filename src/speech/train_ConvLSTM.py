e93d2f0b7683f8897fac50c41f5af748ff37f420
wjyandre <wangjianyou100044@gmail.com>
2019-07-01 11:16:12 -0400

    changes

--- src/speech/train_ConvLSTM.py
+++ src/speech/train_ConvLSTM.py
@@ -31,9 +31,9 @@ scheduler = ReduceLROnPlateau(optimizer=optimizer,factor=0.5, patience=2, thresh
 scheduler2 =CosineAnnealingLR(optimizer2, T_max=300, eta_min=0.0001)
 # Load the training data
 training_data = IEMOCAP(train=True)
-train_loader = DataLoader(dataset=training_data, batch_size=116, shuffle=True, collate_fn=my_collate, num_workers=0)
+train_loader = DataLoader(dataset=training_data, batch_size=100, shuffle=True, collate_fn=my_collate, num_workers=0)
 testing_data = IEMOCAP(train=False)
-test_loader = DataLoader(dataset=testing_data, batch_size=116, shuffle=True, collate_fn=my_collate, num_workers=0)
+test_loader = DataLoader(dataset=testing_data, batch_size=100, shuffle=True, collate_fn=my_collate, num_workers=0)
 
 test_acc=[]
 train_acc=[]
