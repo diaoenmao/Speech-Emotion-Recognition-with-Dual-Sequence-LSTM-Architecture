a64e54acf431339c0e40d63dfa12ae7f5370fab1
wjyandre <wangjianyou100044@gmail.com>
2019-07-02 10:21:55 -0400

    changes

--- src/speech/train_ConvLSTM.py
+++ src/speech/train_ConvLSTM.py
@@ -65,6 +65,7 @@ with torch.no_grad():
         test_case=test_case.float()
         test_case = test_case.unsqueeze(1)
         test_case=torch.split(test_case,int(32000/step),dim=2)
+        pdb.set_trace()
         out = model(test_case, target)
 
         out=torch.flatten(out,start_dim=0,end_dim=1)
