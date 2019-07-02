0081e276fab28c98fc2dc0ae47e764622e64f36f
Michael Xue <michael.xue@duke.edu>
2019-07-02 09:18:54 -0400

    changes

--- src/preprocessing/spectrogram_save_data.py
+++ src/preprocessing/spectrogram_save_data.py
@@ -32,7 +32,7 @@ def extract_features(dataframe):
         index = file.rfind('/')
         basename = file[(index + 1):-4]
         #plt.savefig(endpoint + '{}_spec.png'.format(basename), bbox_inches='tight', pad_inches=0)
-        #print(i)
+        print(i)
         im = cv2.imread(endpoint + '{}_spec.png'.format(basename))
         input.append(im)
         target.append(encode[emotion])
