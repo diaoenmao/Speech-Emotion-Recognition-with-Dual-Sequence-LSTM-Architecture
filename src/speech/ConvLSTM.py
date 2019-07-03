df7bcf8dc9a96fb8c4530c332219007cfc514c7b
wjyandre <wangjianyou100044@gmail.com>
2019-07-03 11:29:28 -0400

    changes

--- src/speech/ConvLSTM.py
+++ src/speech/ConvLSTM.py
@@ -99,9 +99,6 @@ class ConvLSTM(nn.Module):
         for i in range(self.num_devices):
             if seq_length.device.index==i:
                 input=input[int(i*batch_size/self.num_devices):int((i+1)*batch_size/self.num_devices)]
-        for i in input:
-            print(torch.tensor(len(i)).to(self.device))
-        print(len(input))
         temp=[]
         for i in input:
             for k in i:
