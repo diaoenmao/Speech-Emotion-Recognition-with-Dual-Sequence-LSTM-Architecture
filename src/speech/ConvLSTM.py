25c24b2b5767dcee5d8498052afa2299dd637557
wjyandre <wangjianyou100044@gmail.com>
2019-07-03 10:22:35 -0400

    changes

--- src/speech/ConvLSTM.py
+++ src/speech/ConvLSTM.py
@@ -149,9 +149,9 @@ class ConvLSTM(nn.Module):
             losses_batch += loss
         losses_batch=losses_batch/length
         # losses_batch is normalized
-        correct_batch=torch.unsqueeze(correct_batch,dim=0).to(self.device)
-        losses_batch=torch.unsqueeze(losses_batch, dim=0).to(self.device)
-        length=torch.unsqueeze(length,dim=0).to(self.device)
+        correct_batch=torch.unsqueeze(correct_batch,dim=0).float().to(self.device)
+        losses_batch=torch.unsqueeze(losses_batch, dim=0).float().to(self.device)
+        length=torch.unsqueeze(length,dim=0).float().to(self.device)
 
         return  losses_batch, correct_batch, length
 
