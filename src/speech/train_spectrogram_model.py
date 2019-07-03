52e603c2bfc9f77310f56c0f8be492de6a785675
wjyandre <wangjianyou100044@gmail.com>
2019-07-03 18:11:09 -0400

    changes

--- src/speech/train_spectrogram_model.py
+++ src/speech/train_spectrogram_model.py
@@ -83,6 +83,7 @@ for epoch in range(200):  # again, normally you would NOT do 300 epochs, it is t
     train_loss.append(losses)
     print("Epoch: {}-----------Training Loss: {} -------- Testing Loss: {} -------- Training Acc: {} -------- Testing Acc: {}".format(epoch+1,losses,losses_test, accuracy, accuracy_test)+"\n")
     with open("/scratch/speech/models/classification/spectrogram_stats.txt","a+") as f:
+        if epoch==0: f.write("\n"+"============================== New Model ==================================="+"\n")
         f.write("Epoch: {}-----------Training Loss: {} -------- Testing Loss: {} -------- Training Acc: {} -------- Testing Acc: {}".format(epoch+1,losses,losses_test, accuracy, accuracy_test)+"\n")
 
 
