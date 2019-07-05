3f991e0d85c89acc8d96f11c433b1f05631c57ce
wjyandre <wangjianyou100044@gmail.com>
2019-07-05 15:17:32 -0400

    changes

--- src/speech/train_convLSTM_spectrogram.py
+++ src/speech/train_convLSTM_spectrogram.py
@@ -12,15 +12,20 @@ import pickle
 import numpy as np
 
 path="/scratch/speech/models/classification/ConvLSTM_data_debug.pickle"
+
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 hidden_channels=[64,32,16]
-kernel_size=[9,5,5]
+kernel_size=[(9,5),(5,5),(5,5)]
+kernel_size_pool=[(8,8),(8,4),(5,5)]
+kernel_stride_pool=[(4,4),(4,2),(3,2)]
 step=10
-model = ConvLSTM(3, hidden_channels,kernel_size,step,True)
+batch_size=100
+
+model = ConvLSTM(3, hidden_channels,kernel_size,kernel_size_pool,kernel_stride_pool,step,device)
 print("============================ Number of parameters ====================================")
 print(str(sum(p.numel() for p in model.parameters() if p.requires_grad)))
 model.cuda()
-device_ids=[0,1]
+device_ids=[0,1,2,3]
 num_devices=len(device_ids)
 model=DataParallel(model,device_ids=device_ids)
 model.train()
@@ -34,10 +39,10 @@ scheduler2 =CosineAnnealingLR(optimizer2, T_max=300, eta_min=0.0001)
 
 
 # Load the training data
-training_data = IEMOCAP(train=True, segment=True)
-train_loader = DataLoader(dataset=training_data, batch_size=200, shuffle=True, collate_fn=my_collate, num_workers=0, drop_last=True)
-testing_data = IEMOCAP(train=False, segment=True)
-test_loader = DataLoader(dataset=testing_data, batch_size=100, shuffle=True, collate_fn=my_collate, num_workers=0)
+training_data = IEMOCAP(train=True)
+train_loader = DataLoader(dataset=training_data, batch_size=batch_size, shuffle=True, collate_fn=my_collate, num_workers=0, drop_last=True)
+testing_data = IEMOCAP(train=False)
+test_loader = DataLoader(dataset=testing_data, batch_size=batch_size, shuffle=True, collate_fn=my_collate, num_workers=0,drop_last=True)
 print("=================")
 print(len(training_data))
 print("===================")
@@ -46,77 +51,52 @@ train_acc=[]
 test_loss=[]
 train_loss=[]
 epoch=0
-torch.save(model.module.state_dict(), "/scratch/speech/models/classification/ConvLSTM_checkpoint_epoch_{}.pt".format(epoch))
-for epoch in range(300):  # again, normally you would NOT do 300 epochs, it is toy data
+for epoch in range(10):  # again, normally you would NOT do 300 epochs, it is toy data
     print("===================================" + str(epoch+1) + "==============================================")
     losses = 0
     correct=0
-    losses_test = 0
-    correct_test = 0
     model.train()
-    for j, (input, target, _) in enumerate(train_loader):
+    for j, (input, target) in enumerate(train_loader):
         if (j+1)%5==0: print("================================= Batch"+ str(j+1)+ "===================================================")
 
         model.zero_grad()
-        out, loss = model(input, target)
-        #pdb.set_trace()
-        loss = torch.mean(loss,dim=0)
-        out=torch.flatten(out,start_dim=0,end_dim=1)
-        #pdb.set_trace()
-        losses += loss.item() * target.shape[0]
+        losses_batch,correct_batch= model(input, target)
+        loss = torch.mean(losses_batch,dim=0)
+        correct_batch=torch.sum(correct_batch,dim=0)
+        losses += loss.item() * batch_size
         loss.backward()
         optimizer.step()
+        correct += correct_batch.item()
+    accuracy=correct*1.0/((j+1)*batch_size)
+    losses=losses / ((j+1)*batch_size)
 
-        index = torch.argmax(out, dim=1)
-        target_index = torch.argmax(target, dim=1).to(device)
-        correct += sum(index == target_index).item()
-    accuracy=correct*1.0/(len(training_data))
-    losses=losses / (len(training_data))
-
-    # we save the model, and run it on single gpu
-    torch.save(model.module.state_dict(), "/scratch/speech/models/classification/ConvLSTM_checkpoint_epoch_{}.pt".format(epoch+1))
-    model1=ConvLSTM(1, hidden_channels,kernel_size,step,True)
-    model1.load_state_dict(torch.load("/scratch/speech/models/classification/ConvLSTM_checkpoint_epoch_{}.pt".format(epoch+1)))
-    model1.eval()
-    with torch.cuda.device(0):
-        model1.cuda()
+    losses_test = 0
+    correct_test = 0
+    model.eval()
     with torch.no_grad():
-        for test_case, target, seq_length in test_loader:
-            temp=[]
-            for i in test_case:
-                for k in i:
-                    temp.append(k)
-            test_case=torch.from_numpy(np.array([i for i in temp])).to(device)
-            length=test_case.shape[0]
+        for j,(input, target) in enumerate(test_loader):
+            losses_batch,correct_batch = model(input, target)
+            loss = torch.mean(losses_batch,dim=0)
+            correct_batch=torch.sum(correct_batch,dim=0)
+            losses_test += loss.item() * batch_size
+            correct_test += correct_batch.item()
 
-            test_case=test_case.float()
-            test_case = test_case.unsqueeze(1)
-            test_case=torch.split(test_case,int(32000/step),dim=2)
-            out,_ = model1(test_case, target, False)
-            target_index = torch.argmax(target, dim=1).to(device)
-            temp=0
-            temp1=0
-            for i,j in enumerate(target_index):
-                temp1+=seq_length[i].item()
-                if j==torch.argmax(torch.sum(out[temp:temp1,:],dim=0)):
-                    correct_test+=1
-                temp=temp1
 
-    accuracy_test = correct_test * 1.0 / (len(testing_data))
-    #if losses_test<0.95: scheduler=scheduler2; optimizer=optimizer2
+    accuracy_test = correct_test * 1.0 / ((j+1)*batch_size)
+    losses_test = losses_test / ((j+1)*batch_size)
 
     # data gathering
     test_acc.append(accuracy_test)
     train_acc.append(accuracy)
+    test_loss.append(losses_test)
     train_loss.append(losses)
-    print("Epoch: {}-----------Training Loss: {}  -------- Training Acc: {} -------- Testing Acc: {}".format(epoch+1,losses, accuracy, accuracy_test)+"\n")
-    with open("/scratch/speech/models/classification/ConvLSTM_checkpoint_stats.txt","a+") as f:
-        if epoch==0: f.write("+\n"+"=============================  Begining New Model ======================================================="+"\n")
-        f.write("Epoch: {}-----------Training Loss: {} -------- Training Acc: {} -------- Testing Acc: {}".format(epoch+1,losses, accuracy, accuracy_test)+"\n")
-    scheduler.step(losses)
-    #scheduler2.step()
+    print("Epoch: {}-----------Training Loss: {} -------- Testing Loss: {} -------- Training Acc: {} -------- Testing Acc: {}".format(epoch+1,losses,losses_test, accuracy, accuracy_test)+"\n")
+    with open("/scratch/speech/models/classification/ConvLSTM_spectrogram_stats.txt","a+") as f:
+        if epoch==0: f.write("\n"+"============================== New Model ==================================="+"\n")
+        f.write("Epoch: {}-----------Training Loss: {} -------- Testing Loss: {} -------- Training Acc: {} -------- Testing Acc: {}".format(epoch+1,losses,losses_test, accuracy, accuracy_test)+"\n")
 
 
-pickle_out=open("/scratch/speech/models/classification/ConvLSTM_checkpoint_stats.pkl","wb")
-pickle.dump({"test_acc":test_acc, "train_acc": train_acc, "train_loss": train_loss},pickle_out)
+
+pickle_out=open("/scratch/speech/models/classification/ConvLSTM_spectrogram_checkpoint_stats.pkl","wb")
+pickle.dump({"test_acc":test_acc, "train_acc": train_acc, "train_loss": train_loss,"test_loss":test_loss},pickle_out)
 pickle_out.close()
