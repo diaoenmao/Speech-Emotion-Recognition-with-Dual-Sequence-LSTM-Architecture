efea6f5581bff778f57381e72a844ecb61c52164
wjyandre <wangjianyou100044@gmail.com>
2019-07-09 22:39:26 -0400

    changes

--- src/speech/train_joint.py
+++ src/speech/train_joint.py
@@ -30,8 +30,6 @@ model.train()
 
 # Use Adam as the optimizer with learning rate 0.01 to make it fast for testing purposes
 optimizer = optim.Adam({"params":model.parameters()},{"params": model.module.parameters()["weight"],"lr":0.01}, lr=0.001)
-{'params': net.layer.weight},
-                {'params': net.layer.bias, 'lr': 0.01}
 optimizer2=optim.SGD(model.parameters(), lr=0.1)
 scheduler = ReduceLROnPlateau(optimizer=optimizer,factor=0.5, patience=2, threshold=1e-3)
 #scheduler2=ReduceLROnPlateau(optimizer=optimizer2, factor=0.5, patience=2, threshold=1e-3)
