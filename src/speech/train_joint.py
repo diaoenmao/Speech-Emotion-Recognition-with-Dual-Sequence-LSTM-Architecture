342944e54ab5ec4463d64dae6562a5be6e884206
wjyandre <wangjianyou100044@gmail.com>
2019-07-10 19:50:27 -0400

    changes

--- src/speech/train_joint.py
+++ src/speech/train_joint.py
@@ -14,9 +14,9 @@ from sklearn.metrics import confusion_matrix
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 input_channels=3
 hidden_channels=[64,128,32]
-kernel_size=[(3,3),(3,3),(2,2)]
+kernel_size=[(3,3),(3,3),(3,3)]
 kernel_size_pool=[(3,3),(3,3),(3,2)]
-kernel_stride_pool=[(2,2),(2,2),(2,2)]
+kernel_stride_pool=[(2,2),(2,2),(3,2)]
 step=40
 batch_size=60
 hidden_dim_lstm=200
