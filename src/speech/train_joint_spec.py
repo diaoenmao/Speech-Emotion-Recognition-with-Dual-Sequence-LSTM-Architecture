c7c19b860facb57d55f3cca0e1fdb6015ded55d7
wjyandre <wangjianyou100044@gmail.com>
2019-07-11 17:30:44 -0400

    changes

--- src/speech/train_joint_spec.py
+++ src/speech/train_joint_spec.py
@@ -22,7 +22,7 @@ hidden_channels=[32,16]
 kernel_size=[7,7]
 kernel_size_pool=[3,3]
 kernel_stride_pool=[2,2]
-batch_size=100
+batch_size=40
 hidden_dim_lstm=200
 num_layers_lstm=2
 device_ids=[0]
@@ -72,7 +72,6 @@ for epoch in range(100):  # again, normally you would NOT do 300 epochs, it is t
     for j, (input_lstm,input, target,seq_length,segment_labels) in enumerate(train_loader):
         if (j+1)%20==0: print("=================================Train Batch"+ str(j+1)+ str(weight)+"===================================================")
         model.zero_grad()
-        print(input.shape)
         losses_batch,correct_batch= model(input_lstm,input, target,seq_length,segment_labels)
         loss = torch.mean(losses_batch,dim=0)
         correct_batch=torch.sum(correct_batch,dim=0)
