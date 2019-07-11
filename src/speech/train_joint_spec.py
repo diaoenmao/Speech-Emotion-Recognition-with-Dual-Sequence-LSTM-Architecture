e533d77be0807b51e9642a1fe73a94d25ce4b0a8
wjyandre <wangjianyou100044@gmail.com>
2019-07-11 17:14:57 -0400

    changes

--- src/speech/train_joint_spec.py
+++ src/speech/train_joint_spec.py
@@ -22,10 +22,10 @@ hidden_channels=[32,16]
 kernel_size=[7,7]
 kernel_size_pool=[3,3]
 kernel_stride_pool=[2,2]
-batch_size=40
+batch_size=10
 hidden_dim_lstm=200
 num_layers_lstm=2
-device_ids=[0,1,2,3]
+device_ids=[0]
 num_devices=len(device_ids)
 model = ConvLSTM(input_channels,hidden_channels,kernel_size,kernel_size_pool,kernel_stride_pool,device,num_devices,hidden_dim_lstm,num_layers_lstm)
 print("============================ Number of parameters ====================================")
