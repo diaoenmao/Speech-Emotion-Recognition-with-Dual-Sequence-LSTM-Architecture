4820b788a2a4350c684386c48f29259abb678300
wjyandre <wangjianyou100044@gmail.com>
2019-07-10 21:35:40 -0400

    changes

--- src/speech/model_joint.py
+++ src/speech/model_joint.py
@@ -126,7 +126,7 @@ class ConvLSTM(nn.Module):
         self.attention=nn.Parameter(torch.zeros(self.linear_dim))
         self.attention_flag=attention_flag
 
-        self.weight= nn.Parameter(torch.tensor(0.01).float())
+        self.weight= nn.Parameter(torch.tensor(-0.01).float())
 
 
 
