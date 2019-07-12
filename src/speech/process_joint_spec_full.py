5ee2e89b31c876a6219cc489a279dfd80f40dd0c
Michael Xue <michael.xue@duke.edu>
2019-07-12 16:38:55 -0400

    changes

--- src/speech/process_joint_spec_full.py
+++ src/speech/process_joint_spec_full.py
@@ -78,6 +78,7 @@ class IEMOCAP(Dataset):
     def __getitem__(self, index):
         #temp=[torch.unsqueeze(torch.from_numpy(i),dim=3).permute(2,0,1,3) for i in self.input[index]]
         #temp=[torch.from_numpy(i).permute(1,0).float() for i in self.input[index]]
+        pdb.set_trace()
         sample = {'input_lstm': torch.from_numpy(self.input_lstm[index]).float(),
                   'seq_length': int(self.seq_length[index]),
                   'input': self.input[index],
