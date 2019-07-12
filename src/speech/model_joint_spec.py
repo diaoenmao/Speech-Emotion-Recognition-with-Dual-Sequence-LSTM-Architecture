a5ac90beb2d57737b8f89388ce6587ac57714529
wjyandre <wangjianyou100044@gmail.com>
2019-07-11 21:01:43 -0400

    changes

--- src/speech/model_joint_spec.py
+++ src/speech/model_joint_spec.py
@@ -130,9 +130,9 @@ class ConvLSTM(nn.Module):
     def forward(self, input_lstm,input,target,seq_length, segment_labels,train=True):
         # input should be a list of inputs, like a time stamp, maybe 1280 for 100 times.
         ##data process here
-        #b_size=input.shape[0]
-        pdb.set_trace()
-        #segment_labels=segment_labels[int(input.device.index*batch_size/self.num_devices):int((input.device.index+1)*batch_size/self.num_devices)]
+        batch_size=input.shape[0]
+        segment_labels=segment_labels[int(input.device.index*batch_size/self.num_devices):int((input.device.index+1)*batch_size/self.num_devices)]
+        print(input.device.index,segment_labels)
         internal_state = []
         outputs = []
         step=input.shape[1]
