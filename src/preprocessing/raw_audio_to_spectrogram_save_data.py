4773a695763aec0cd9808adbb59785562035595c
Michael Xue <michael.xue@duke.edu>
2019-07-12 11:06:28 -0400

    changes

--- src/preprocessing/raw_audio_to_spectrogram_save_data.py
+++ src/preprocessing/raw_audio_to_spectrogram_save_data.py
@@ -16,8 +16,9 @@ def create_data(data):
     utterances_new_mel = []
     for i, utterance in enumerate(data['input']):
         plt.clf()
-        lin_spectrogram, freqs, t, im = plt.specgram(utterance, Fs=sample_rate, NFFT=nfft)
-        mel_spectrogram = librosa.feature.melspectrogram(utterance.astype('float'), sr=sample_rate, n_fft=nfft)
+        lin_spectrogram, freqs, t, im = plt.specgram(y=utterance, Fs=sample_rate, NFFT=nfft)
+        #mel_spectrogram = librosa.feature.melspectrogram(utterance.astype('float'), sr=sample_rate, n_fft=nfft)
+        mel_spectrogram = librosa.feature.melspectrogram(S=lin_spectrogram, n_fft=nfft)
         utterances_new.append(lin_spectrogram)
         utterances_new_mel.append(mel_spectrogram)
         print(i)
