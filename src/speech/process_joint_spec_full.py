809c43ab9c3b6149cdeb517f9ad53eafd8e02cfc
Michael Xue <michael.xue@duke.edu>
2019-07-12 13:05:33 -0400

    changes

--- src/speech/process_joint_spec_full.py
+++ src/speech/process_joint_spec_full.py
@@ -99,8 +99,9 @@ def my_collate(batch):
     target=torch.from_numpy(np.array(target))
     input_lstm = pad_sequence(sequences=input_lstm,batch_first=True)
     #input=pad_sequence(input,batch_first=True)
+    input = [torch.unsqueeze(i, dim=0) for i in input]
+    input = torch.cat(input, dim=0))
     pdb.set_trace()
-    input = torch.Tensor(input)
     input = torch.unsqueeze(input, dim=1)
     input = input.permute(0,1,3,2).float()
 
