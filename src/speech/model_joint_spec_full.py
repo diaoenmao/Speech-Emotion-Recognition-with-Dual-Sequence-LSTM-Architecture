04c029cf63c125cdc2c387648b79e28b0893ef50
Michael Xue <michael.xue@duke.edu>
2019-07-12 13:33:35 -0400

    changes

--- src/speech/model_joint_spec_full.py
+++ src/speech/model_joint_spec_full.py
@@ -114,8 +114,9 @@ class SpectrogramModel(nn.Module):
 
         out_lstm=self.LSTM_Audio(input_lstm).permute(0,2,1)
         out=torch.mean(out,dim=2)
-        temp=[torch.unsqueeze(torch.mean(out_lstm[k,:,:s],dim=1),dim=0) for k,s in enumerate(seq_length)]
-        out_lstm=torch.cat(temp,dim=0)
+        #essentially pack_padded_sequence in order to work on dataparallel
+        #temp=[torch.unsqueeze(torch.mean(out_lstm[k,:,:s],dim=1),dim=0) for k,s in enumerate(seq_length)]
+        #out_lstm=torch.cat(temp,dim=0)
         p=torch.exp(10*self.weight)/(1+torch.exp(10*self.weight))
         out=torch.cat([p*out,(1-p)*out_lstm],dim=1)
         out=self.classification(out)
