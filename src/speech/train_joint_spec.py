61e1e76f73c5d454dbb89bab6c44ff6d85820c95
wjyandre <wangjianyou100044@gmail.com>
2019-07-11 20:49:32 -0400

    changes

--- src/speech/train_joint_spec.py
+++ src/speech/train_joint_spec.py
@@ -22,10 +22,10 @@ hidden_channels=[32,16]
 kernel_size=[7,7]
 kernel_size_pool=[3,3]
 kernel_stride_pool=[2,2]
-batch_size=40
+batch_size=4
 hidden_dim_lstm=200
 num_layers_lstm=2
-device_ids=[0]
+device_ids=[0,1,2,3]
 num_devices=len(device_ids)
 model = ConvLSTM(input_channels,hidden_channels,kernel_size,kernel_size_pool,kernel_stride_pool,device,num_devices,hidden_dim_lstm,num_layers_lstm)
 print("============================ Number of parameters ====================================")
@@ -72,6 +72,7 @@ for epoch in range(100):  # again, normally you would NOT do 300 epochs, it is t
     for j, (input_lstm,input, target,seq_length,segment_labels) in enumerate(train_loader):
         if (j+1)%20==0: print("=================================Train Batch"+ str(j+1)+ str(weight)+"===================================================")
         model.zero_grad()
+        pdb.set_trace()
         losses_batch,correct_batch= model(input_lstm,input, target,seq_length,segment_labels)
         loss = torch.mean(losses_batch,dim=0)
         correct_batch=torch.sum(correct_batch,dim=0)
