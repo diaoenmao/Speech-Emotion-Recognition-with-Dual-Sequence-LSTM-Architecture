890a5155685b0acbddc765556b93f7ee7c247489
Michael Xue <michael.xue@duke.edu>
2019-07-17 13:03:15 -0400

    changes

--- src/speech/model_joint_spec_full.py
+++ src/speech/model_joint_spec_full.py
@@ -125,7 +125,7 @@ class SpectrogramModel(nn.Module):
                 temp.append(s)
         seq_length_spec=torch.Tensor(temp)
         #out = torch.flatten(x,start_dim=1,end_dim=2).permute(0,2,1)
-        out = out.permute(0,2,1)
+        out = x.permute(0,2,1)
         out, hn = self.lstm(out)
         out=out.permute(0,2,1)
         out_lstm=self.LSTM_Audio(input_lstm).permute(0,2,1)
