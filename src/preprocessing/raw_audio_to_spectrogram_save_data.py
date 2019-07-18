75c8b06efc1ecd08c224ab3f48211593afa7470a
Michael Xue <michael.xue@duke.edu>
2019-07-18 12:21:23 -0400

    changes

--- src/preprocessing/raw_audio_to_spectrogram_save_data.py
+++ src/preprocessing/raw_audio_to_spectrogram_save_data.py
@@ -19,22 +19,28 @@ def create_data(train_data, test_data, n_fft):
     utterances_new_test = []
     utterances_new_mel_test = []
 
-    for i, (utterance_train, utterance_test) in enumerate(zip(train_data['input'], test_data['input'])):
+    for i, utterance_train in enumerate(train_data['input']):
         plt.clf()
         lin_spectrogram_train, freqs, t, im = plt.specgram(utterance_train, Fs=sample_rate, NFFT=n_fft)
-        lin_spectrogram_test, freqs, t, im = plt.specgram(utterance_test, Fs=sample_rate, NFFT=n_fft)
         mel_spectrogram_train = librosa.feature.melspectrogram(y=utterance_train.astype('float'), sr=sample_rate, n_fft=n_fft)
-        mel_spectrogram_test = librosa.feature.melspectrogram(y=utterance_test.astype('float'), sr=sample_rate, n_fft=n_fft)
         #mel_spectrogram = librosa.feature.melspectrogram(S=lin_spectrogram, n_fft=nfft)
         utterances_new_train.append(lin_spectrogram_train)
         utterances_new_mel_train.append(mel_spectrogram_train)
+        print(i)
+    print('Parsing through train data done.')
+    for i, utterance_test in enumerate(test_data['input']):
+        plt.clf()
+        lin_spectrogram_test, freqs, t, im = plt.specgram(utterance_test, Fs=sample_rate, NFFT=n_fft)
+        mel_spectrogram_test = librosa.feature.melspectrogram(y=utterance_test.astype('float'), sr=sample_rate, n_fft=n_fft)
+        #mel_spectrogram = librosa.feature.melspectrogram(S=lin_spectrogram, n_fft=nfft)
         utterances_new_test.append(lin_spectrogram_test)
         utterances_new_mel_test.append(mel_spectrogram_test)
         print(i)
+    print('Parsing through test data done.')
     return utterances_new_train, utterances_new_mel_train, utterances_new_test, utterances_new_mel_test
 
 if __name__ == '__main__':
-    utterances_new_train, utterances_new_mel_train, utterances_new_test, utterancs_new_mel_test = create_data(train_data, test_data, n_fft=nfft)
+    utterances_new_train, utterances_new_mel_train, utterances_new_test, utterances_new_mel_test = create_data(train_data, test_data, n_fft=nfft)
 
     input_lstm_train = train_data['input_lstm']
     target_train = train_data['target']
