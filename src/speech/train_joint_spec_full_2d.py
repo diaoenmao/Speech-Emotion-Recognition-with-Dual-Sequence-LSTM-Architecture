f55d3eba78f3a2cf2f721da321e6d908fc278506
wjyandre <wangjianyou100044@gmail.com>
2019-07-18 14:39:07 -0400

    changes

--- src/speech/train_joint_spec_full_2d.py
+++ src/speech/train_joint_spec_full_2d.py
@@ -37,7 +37,7 @@ model.train()
 
 # Use Adam as the optimizer with learning rate 0.01 to make it fast for testing purposes
 optimizer = optim.Adam(model.parameters(),lr=0.001)
-optimizer2=optim.SGD(model.parameters(), lr=0.01)
+optimizer2=optim.SGD(model.parameters(), lr=0.1, momentum=0.9)
 scheduler = ReduceLROnPlateau(optimizer=optimizer,factor=0.5, patience=2, threshold=1e-3)
 #scheduler2=ReduceLROnPlateau(optimizer=optimizer2, factor=0.5, patience=2, threshold=1e-3)
 #scheduler2 =CosineAnnealingLR(optimizer2, T_max=300, eta_min=0.0001)
