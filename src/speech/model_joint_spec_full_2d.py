65d5f476b22ceb1755a4f82b153d60bebe495bb6
wjyandre <wangjianyou100044@gmail.com>
2019-07-18 17:31:26 -0400

    changes

--- src/speech/model_joint_spec_full_2d.py
+++ src/speech/model_joint_spec_full_2d.py
@@ -137,9 +137,9 @@ class SpectrogramModel(nn.Module):
         #temp=torch.mean(out_lstm,dim=2)
         out_lstm=torch.cat(temp,dim=0)
         #out=torch.cat(temp1,dim=0)
-        p=torch.exp(10*self.weight)/(1+torch.exp(10*self.weight))
-        out=torch.cat([p*out,(1-p)*out_lstm],dim=1)
-        #out=torch.cat([out,out_lstm],dim=1)
+        #p=torch.exp(10*self.weight)/(1+torch.exp(10*self.weight))
+        #out=torch.cat([p*out,(1-p)*out_lstm],dim=1)
+        out=torch.cat([out,out_lstm],dim=1)
         #out=self.classification_raw(out)
         #out_lstm=self.classification_hand(out_lstm)
         #out_final=p*out+(1-p)*out_lstm
