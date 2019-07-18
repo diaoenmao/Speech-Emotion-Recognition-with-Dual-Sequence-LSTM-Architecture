672796962c92e84e8eac284cda26096ec403ee0f
Michael Xue <michael.xue@duke.edu>
2019-07-18 10:10:44 -0400

    changes

--- src/preprocessing/data_augmentation_1.py
+++ src/preprocessing/data_augmentation_1.py
@@ -31,87 +31,87 @@ emotions = [happy, neutral, angry, sad]
 additions = []
 
 for x, emotion in enumerate(emotions):
-    for utterance in emotion:
 
-        utterances_0_1 = []
-        utterances_1_2 = []
-        utterances_2_3 = []
-        utterances_3_4 = []
-        utterances_4_5 = []
-        utterances_5_6 = []
-        utterances_6_7 = []
-        utterances_7_8 = []
+    utterances_0_1 = []
+    utterances_1_2 = []
+    utterances_2_3 = []
+    utterances_3_4 = []
+    utterances_4_5 = []
+    utterances_5_6 = []
+    utterances_6_7 = []
+    utterances_7_8 = []
 
-        for utterance in data['input']:
-            if len(utterance)/sr < 8:
-                if len(utterance)/sr > 7:
-                    utterances_7_8.append(utterance)
-                elif len(utterance)/sr > 6:
-                    utterances_6_7.append(utterance)
-                elif len(utterance)/sr > 5:
-                    utterances_5_6.append(utterance)
-                elif len(utterance)/sr > 4:
-                    utterances_4_5.append(utterance)
-                elif len(utterance)/sr > 3:
-                    utterances_3_4.append(utterance)
-                elif len(utterance)/sr > 2:
-                    utterances_2_3.append(utterance)
-                elif len(utterance)/sr > 1:
-                    utterances_1_2.append(utterance)
-                else:
-                    utterances_0_1.append(utterance)
+    for y, utterance in enumerate(emotion):
+        print("Sample " + y + " from emotion " + x)
+        if len(utterance)/sr < 8:
+            if len(utterance)/sr > 7:
+                utterances_7_8.append(utterance)
+            elif len(utterance)/sr > 6:
+                utterances_6_7.append(utterance)
+            elif len(utterance)/sr > 5:
+                utterances_5_6.append(utterance)
+            elif len(utterance)/sr > 4:
+                utterances_4_5.append(utterance)
+            elif len(utterance)/sr > 3:
+                utterances_3_4.append(utterance)
+            elif len(utterance)/sr > 2:
+                utterances_2_3.append(utterance)
+            elif len(utterance)/sr > 1:
+                utterances_1_2.append(utterance)
+            else:
+                utterances_0_1.append(utterance)
 
-        matrix_0_8 = []
-        for i in range(len(utterances_0_1)):
-            for j in range(len(utterances_7_8)):
-                matrix_0_8.append((i, j))
+    matrix_0_8 = []
+    for i in range(len(utterances_0_1)):
+        for j in range(len(utterances_7_8)):
+            matrix_0_8.append((i, j))
 
-        matrix_1_7 = []
-        for i in range(len(utterances_1_2)):
-            for j in range(len(utterances_6_7)):
-                matrix_1_7.append((i, j))
+    matrix_1_7 = []
+    for i in range(len(utterances_1_2)):
+        for j in range(len(utterances_6_7)):
+            matrix_1_7.append((i, j))
 
-        matrix_2_6 = []
-        for i in range(len(utterances_2_3)):
-            for j in range(len(utterances_5_6)):
-                matrix_2_6.append((i, j))
+    matrix_2_6 = []
+    for i in range(len(utterances_2_3)):
+        for j in range(len(utterances_5_6)):
+            matrix_2_6.append((i, j))
 
-        matrix_3_5 = []
-        for i in range(len(utterances_3_4)):
-            for j in range(len(utterances_4_5)):
-                matrix_3_5.append((i, j))
+    matrix_3_5 = []
+    for i in range(len(utterances_3_4)):
+        for j in range(len(utterances_4_5)):
+            matrix_3_5.append((i, j))
 
-        rand_0_8 = np.random.choice(matrix_0_8, size=2000)
-        for (i, j) in rand_0_8:
-            additions.append(np.append(utterances_0_1[i], utterances_7_8[j]))
-            a = np.zeros(4)
-            np.put(a, x, 1)
-            target.append(a)
-            seq_length.append(len(utterances_0_1[i] + len(utterances_7_8[j])))
+    rand_0_8 = np.random.choice(matrix_0_8, size=2000)
+    for (i, j) in rand_0_8:
+        additions.append(np.append(utterances_0_1[i], utterances_7_8[j]))
+        a = np.zeros(4)
+        np.put(a, x, 1)
+        target.append(a)
+        seq_length.append(len(utterances_0_1[i] + len(utterances_7_8[j])))
 
-        rand_1_7 = np.random.choice(matrix_1_7, size=2000)
-        for (i, j) in rand_1_7:
-            additions.append(np.append(utterances_1_2[i], utterances_6_7[j]))
-            a = np.zeros(4)
-            np.put(a, x, 1)
-            target.append(a)
-            seq_length.append(len(utterances_1_2[i] + len(utterances_6_7[j])))
+    rand_1_7 = np.random.choice(matrix_1_7, size=2000)
+    for (i, j) in rand_1_7:
+        additions.append(np.append(utterances_1_2[i], utterances_6_7[j]))
+        a = np.zeros(4)
+        np.put(a, x, 1)
+        target.append(a)
+        seq_length.append(len(utterances_1_2[i] + len(utterances_6_7[j])))
 
-        rand_2_6 = np.random.choice(matrix_2_6, size=2000)
-        for (i, j) in rand_2_6:
-            additions.append(np.append(utterances_2_3[i], utterances_5_6[j]))
-            a = np.zeros(4)
-            np.put(a, x, 1)
-            target.append(a)
-            seq_length.append(len(utterances_2_3[i] + len(utterances_5_6[j])))
+    rand_2_6 = np.random.choice(matrix_2_6, size=2000)
+    for (i, j) in rand_2_6:
+        additions.append(np.append(utterances_2_3[i], utterances_5_6[j]))
+        a = np.zeros(4)
+        np.put(a, x, 1)
+        target.append(a)
+        seq_length.append(len(utterances_2_3[i] + len(utterances_5_6[j])))
 
-        rand_3_5 = np.random.choice(matrix_3_5, size=2000)
-        for (i, j) in rand_3_5:
-            additions.append(np.append(utterances_3_4[i], utterances_4_5[j]))
-            a = np.zeros(4)
-            np.put(a, x, 1)
-            target.append(a)
-            seq_length.append(len(utterances_3_4[i] + len(utterances_4_5[j])))
+    rand_3_5 = np.random.choice(matrix_3_5, size=2000)
+    for (i, j) in rand_3_5:
+        additions.append(np.append(utterances_3_4[i], utterances_4_5[j]))
+        a = np.zeros(4)
+        np.put(a, x, 1)
+        target.append(a)
+        seq_length.append(len(utterances_3_4[i] + len(utterances_4_5[j])))
 
 input += additions
 if len(input) == len(target) == len(seq_length):
