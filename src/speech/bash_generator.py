304d6c168d1607d00be2ba97295af2e77c40f43b
wjyandre <wangjianyou100044@gmail.com>
2019-07-19 10:20:04 -0400

    changes

--- src/speech/bash_generator.py
+++ src/speech/bash_generator.py
@@ -1,10 +1,10 @@
-def generate_bash(name,nfft,batch_size,out_channels, kernel_size_cnn, stride_size_cnn, kernel_size_pool,stride_size_pool):
+def generate_bash(namedict,batch_size,out_channels, kernel_size_cnn, stride_size_cnn, kernel_size_pool,stride_size_pool):
     commands = []
     for o in out_channels:
         out1=o[0]
         out2=o[1]
-        for n in name:
-            for fft in nfft:
+        for n in namedict.keys():
+            for fft in namedict[n]:
                 for b in batch_size:
                     for kc in kernel_size_cnn:
                         for sc in stride_size_cnn:
@@ -17,13 +17,12 @@ def generate_bash(name,nfft,batch_size,out_channels, kernel_size_cnn, stride_siz
             f.write(j+"\n")
 
 if __name__ == '__main__':
-    name= ['mel','linear']
-    nfft=[256,512]
+    namedict={"mel":[512,1024],"linear":[256,1024]}
     batch_size = [100]
     out_channels=[[64,16],[128,32],[256,16],[16,64],[32,128],[16,256]]
     kernel_size_cnn=[2,3,4]
     stride_size_cnn=[1]
     kernel_size_pool=[2,4]
     stride_size_pool=[2,4]
-    generate_bash(name,nfft,batch_size,out_channels, kernel_size_cnn, stride_size_cnn, kernel_size_pool,stride_size_pool)
+    generate_bash(namedict,batch_size,out_channels, kernel_size_cnn, stride_size_cnn, kernel_size_pool,stride_size_pool)
     print("Success")
