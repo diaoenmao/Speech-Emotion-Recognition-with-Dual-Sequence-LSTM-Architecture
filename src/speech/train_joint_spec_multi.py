add50a56dda503b1aeb96a00af15666541e80588
Michael Xue <michael.xue@duke.edu>
2019-07-19 17:51:05 -0400

    changes

--- src/speech/train_joint_spec_multi.py
+++ src/speech/train_joint_spec_multi.py
@@ -51,9 +51,9 @@ scheduler = ReduceLROnPlateau(optimizer=optimizer,factor=0.5, patience=2, thresh
 scheduler3 =MultiStepLR(optimizer, [5,10,15],gamma=0.1)
 
 # Load the training data
-training_data = IEMOCAP(name='mel', nfft=[512, 1024, 2048], train=True)
+training_data = IEMOCAP(name='linear', nfft=[512, 1024, 2048], train=True)
 train_loader = DataLoader(dataset=training_data, batch_size=batch_size, shuffle=True, collate_fn=my_collate, num_workers=0,drop_last=True)
-testing_data = IEMOCAP(name='mel', nfft=[512, 1024, 2048], train=False)
+testing_data = IEMOCAP(name='linear', nfft=[512, 1024, 2048], train=False)
 test_loader = DataLoader(dataset=testing_data, batch_size=batch_size, shuffle=True, collate_fn=my_collate, num_workers=0,drop_last=True)
 
 print("=================")
