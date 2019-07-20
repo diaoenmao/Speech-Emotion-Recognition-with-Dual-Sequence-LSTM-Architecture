cccea16197541a8a0b465b473eef73cce9dc13fa
Michael Xue <michael.xue@duke.edu>
2019-07-20 14:50:49 -0400

    changes

--- src/speech/model_joint_spec_multi.py
+++ src/speech/model_joint_spec_multi.py
@@ -157,8 +157,8 @@ class MultiSpectrogramModel(nn.Module):
         self.classification_raw = nn.Sequential(
                                 nn.Linear(self.hidden_dim_lstm*self.num_directions*self.num_branches, self.hidden_dim_lstm*self.num_directions*self.num_branches//2),
                                 nn.ReLU(),
-                                nn.linear(self.hidden_dim_lstm*self.num_directions*self.num_branches//2,self.num_labels)).to(self.device)
-        
+                                nn.Linear(self.hidden_dim_lstm*self.num_directions*self.num_branches//2,self.num_labels)).to(self.device)
+
         #self.classification_raw=nn.Linear(self.hidden_dim*self.num_directions*self.num_branches,self.num_labels).to(self.device)
         self.weight= nn.Parameter(torch.FloatTensor([0]),requires_grad=False)
 
