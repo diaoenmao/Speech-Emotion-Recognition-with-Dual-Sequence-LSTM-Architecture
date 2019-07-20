060165ff12c8430f12679cf59e3082cb01ab35fd
Michael Xue <michael.xue@duke.edu>
2019-07-20 14:15:17 -0400

    changes

--- src/speech/process_joint_spec_multi.py
+++ src/speech/process_joint_spec_multi.py
@@ -7,24 +7,24 @@ import pdb
 from sklearn.model_selection import train_test_split
 import torch.nn.functional as F
 
-'''
+
 def split_data(data):
-    input_train, input_test, target_train, target_test, input_lstm_train, input_lstm_test, seq_length_train, seq_length_test = train_test_split(
-        data['input'], data['target'], data["input_lstm"],data["seq_length"],test_size=0.2, random_state=42)
-    train = {'input': input_train, 'target': target_train,"input_lstm": input_lstm_train,"seq_length": seq_length_train}
-    test = {'input': input_test, 'target': target_test,"input_lstm": input_lstm_test,"seq_length": seq_length_test}
+    input_train, input_test, target_train, target_test, input_lstm_train, input_lstm_test = train_test_split(
+        data['input'], data['target'], data["input_lstm"],test_size=0.2, random_state=42)
+    train = {'input': input_train, 'target': target_train,"input_lstm": input_lstm_train}
+    test = {'input': input_test, 'target': target_test,"input_lstm": input_lstm_test}
     return train, test
 
 def combine(name, nfft):
     with open('/scratch/speech/datasets/IEMOCAP_39_FOUR_EMO_full.pkl', 'rb') as out1:
         dict1=pickle.load(out1)
 
-    with open('/scratch/speech/raw_audio_dataset/'+name+'_spectrogram_nfft{}_full.pkl'.format(nfft), 'rb') as out2:
+    with open('/scratch/speech/raw_audio_dataset/'+name+'_spectrogram_nfft{}_updated_full.pkl'.format(nfft), 'rb') as out2:
         dict2=pickle.load(out2)
 
 
-    with open('/scratch/speech/raw_audio_dataset/spectrogram_full.pkl', 'rb') as out2:
-        dict2=pickle.load(out2)
+    #with open('/scratch/speech/raw_audio_dataset/spectrogram_full.pkl', 'rb') as out2:
+        #dict2=pickle.load(out2)
 
     flag=True
     for i in range(len(dict1["target"])):
@@ -42,17 +42,17 @@ def combine(name, nfft):
         #eq_length_time.append([j.shape[1] for j in i])
 
 
-    dict3={"input_lstm":dict1["input"],"input":dict2["input"],"seq_length": dict1["seq_length"],"target": dict2["target"]}
+    dict3={"input_lstm":dict1["input"],"input":dict2["input"],"target": dict2["target"]}
     train1,test1=split_data(dict3)
-    with open('/scratch/speech/hand_raw_dataset/EMO39_spec_full.pkl', 'wb') as full:
+    with open('/scratch/speech/hand_raw_dataset/EMO39_'+name+'_spectrogram_nfft{}_updated_full.pkl'.format(nfft), 'wb') as full:
         pickle.dump(dict3,full)
-    with open('/scratch/speech/hand_raw_dataset/EMO39_spec_train.pkl', 'wb') as train:
+    with open('/scratch/speech/hand_raw_dataset/EMO39_'+name+'_spectrogram_nfft{}_updated_train.pkl'.format(nfft), 'wb') as train:
         pickle.dump(train1,train)
-    with open('/scratch/speech/hand_raw_dataset/EMO39_spec_test.pkl', 'wb') as test:
+    with open('/scratch/speech/hand_raw_dataset/EMO39_'+name+'_spectrogram_nfft{}_updated_test.pkl'.format(nfft), 'wb') as test:
         pickle.dump(test1,test)
 
-    print('/scratch/speech/hand_raw_dataset/EMO39_spec_full.pkl')
-'''
+    print('Done.')
+
 
 class IEMOCAP(Dataset):
     def __init__(self, name, nfft, train=True):
@@ -118,14 +118,14 @@ def my_collate(batch):
     return input_lstm, input1, input2, target, seq_length
 
 if __name__=="__main__":
-    '''
-    combine("linear", 256)
-    combine("mel", 256)
-    combine("linear", 512)
+
+    #combine("linear", 256)
+    #combine("mel", 256)
+    #combine("linear", 512)
     combine("mel", 512)
-    combine("linear", 1024)
+    #combine("linear", 1024)
     combine("mel", 1024)
-    combine("linear", 2048)
-    combine("mel", 2048)
-    '''
-    combine("linear",256)
+    #combine("linear", 2048)
+    #combine("mel", 2048)
+
+    #combine("linear",256)
