0ba4f813dca559aced09d9d22bbb8d9b806acf82
wjyandre <wangjianyou100044@gmail.com>
2019-07-20 16:46:46 -0400

    changes

--- src/speech/bash_generator.py
+++ src/speech/bash_generator.py
@@ -1,28 +1,31 @@
-def generate_bash(namedict,batch_size,out_channels, kernel_size_cnn, stride_size_cnn, kernel_size_pool,stride_size_pool):
+def generate_bash(batch_size,out_channels, kernel_size_cnn, stride_size_cnn, kernel_size_pool,stride_size_pool):
     commands = []
     for o in out_channels:
         out1=o[0]
         out2=o[1]
-        for n in namedict.keys():
-            for fft in namedict[n]:
-                for b in batch_size:
-                    for kc in kernel_size_cnn:
-                        for sc in stride_size_cnn:
-                            for kp in kernel_size_pool:
-                                for sp in stride_size_pool:
-                                    commands.append("python train_joint_spec_full_2d.py -n {} -fft {} -b {} -out1 {} -out2 {} -kc {} -sc {} -kp {} -sp {}".format(n,fft,b, out1,out2, kc,sc,kp,sp))
+        for b in batch_size:
+            for kc in kernel_size_cnn:
+                kc1=kc[0]
+                kc2=kc[1]
+                for sc in stride_size_cnn:
+                    sc1=sc[0]
+                    sc2=sc[1]
+                    for kp in kernel_size_pool:
+                        kp1=kp[0]
+                        kp2=kp[1]
+                        for sp in stride_size_pool:
+                            commands.append("python train_joint_spec_full_2d.py -n {} -fft {} -b {} -out1 {} -out2 {} -kc1 {} -kc2 {} -sc1 {} -sc2 {} -kp1 {} -kp2 {} -sp {}".format(n,fft,b,out1,out2,kc1,kc2,sc1,sc1,kp1,kp2,sp))
     with open('gpu_full_autogen_bash.sh','w+') as f:
         f.write('#!/bin/bash\n')
         for j in commands:
             f.write(j+"\n")
 
 if __name__ == '__main__':
-    namedict={"mel":[512,1024],"linear":[256,1024]}
-    batch_size = [100]
-    out_channels=[[64,16],[128,32],[256,16],[16,64],[32,128],[16,256]]
-    kernel_size_cnn=[2,3,4]
-    stride_size_cnn=[1]
-    kernel_size_pool=[2,4]
-    stride_size_pool=[2,4]
-    generate_bash(namedict,batch_size,out_channels, kernel_size_cnn, stride_size_cnn, kernel_size_pool,stride_size_pool)
+    batch_size = [200]
+    out_channels=[[16,64],[64,16]]
+    kernel_size_cnn=[[4,2],[3,2],[5,3],[2,2],[3,3],[4,4],[2,1],[3,1],[4,1]]
+    stride_size_cnn=[[1,1],[2,2],[2,1]]
+    kernel_size_pool=[[2,2]]
+    stride_size_pool=[2,2]
+    generate_bash(batch_size,out_channels, kernel_size_cnn, stride_size_cnn, kernel_size_pool,stride_size_pool)
     print("Success")
