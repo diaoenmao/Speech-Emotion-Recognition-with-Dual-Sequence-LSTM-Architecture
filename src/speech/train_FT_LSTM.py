9636b0bb482b697fca75bbbf08964afed3b2e037
wjyandre <wangjianyou100044@gmail.com>
2019-07-23 00:35:54 -0400

    changes

--- src/speech/train_FT_LSTM.py
+++ src/speech/train_FT_LSTM.py
@@ -102,7 +102,6 @@ def train_model(args):
             losses += loss.item() * batch_size
             loss.backward()
             weight=model.module.state_dict()["weight"]
-            weight=torch.exp(10*weight)/(1+torch.exp(10*weight)).item()
             optimizer.step()
             correct += correct_batch.item()
         accuracy=correct*1.0/((j+1)*batch_size)
