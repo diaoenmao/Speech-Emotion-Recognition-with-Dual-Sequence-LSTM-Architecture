87482de451af35b7eed1962b5efaceff5c56658c
wjyandre <wangjianyou100044@gmail.com>
2019-07-23 00:02:02 -0400

    changes

--- src/speech/gpu_full_autogen_bash_weight2.py
+++ src/speech/gpu_full_autogen_bash_weight2.py
@@ -27,3 +27,11 @@ python train_joint_spec_multi2.py -b 128 -out1 128 -out2 16 -kc1 3 -kc2 3 -sc1 1
 python train_joint_spec_multi2.py -b 128 -out1 128 -out2 16 -kc1 3 -kc2 3 -sc1 1 -sc2 1 -kp1 2 -kp2 2 -sp 2 -w 0.7
 python train_joint_spec_multi2.py -b 128 -out1 128 -out2 16 -kc1 3 -kc2 3 -sc1 1 -sc2 1 -kp1 2 -kp2 2 -sp 2 -w 0.75
 python train_joint_spec_multi2.py -b 128 -out1 128 -out2 16 -kc1 3 -kc2 3 -sc1 1 -sc2 1 -kp1 2 -kp2 2 -sp 2 -w 0.8
+python train_joint_spec_multi.py -b 128 -out1 64 -out2 32 -kc1 4 -kc2 4 -sc1 1 -sc2 1 -kp1 2 -kp2 2 -sp 2 -w 0.4
+python train_joint_spec_multi.py -b 128 -out1 64 -out2 32 -kc1 4 -kc2 4 -sc1 1 -sc2 1 -kp1 2 -kp2 2 -sp 2 -w 0.45
+python train_joint_spec_multi.py -b 128 -out1 64 -out2 32 -kc1 4 -kc2 4 -sc1 1 -sc2 1 -kp1 2 -kp2 2 -sp 2 -w 0.55
+python train_joint_spec_multi.py -b 128 -out1 64 -out2 32 -kc1 4 -kc2 4 -sc1 1 -sc2 1 -kp1 2 -kp2 2 -sp 2 -w 0.6
+python train_joint_spec_multi.py -b 128 -out1 128 -out2 16 -kc1 3 -kc2 3 -sc1 1 -sc2 1 -kp1 2 -kp2 2 -sp 2 -w 0.4
+python train_joint_spec_multi.py -b 128 -out1 128 -out2 16 -kc1 3 -kc2 3 -sc1 1 -sc2 1 -kp1 2 -kp2 2 -sp 2 -w 0.45
+python train_joint_spec_multi.py -b 128 -out1 128 -out2 16 -kc1 3 -kc2 3 -sc1 1 -sc2 1 -kp1 2 -kp2 2 -sp 2 -w 0.55
+python train_joint_spec_multi.py -b 128 -out1 128 -out2 16 -kc1 3 -kc2 3 -sc1 1 -sc2 1 -kp1 2 -kp2 2 -sp 2 -w 0.6
