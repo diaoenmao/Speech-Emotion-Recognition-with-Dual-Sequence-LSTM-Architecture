5c3e95037606863b47e790fff5c989558a80f422
wjyandre <wangjianyou100044@gmail.com>
2019-07-23 11:33:14 -0400

    changes

--- src/speech/process_FT_LSTM.py
+++ src/speech/process_FT_LSTM.py
@@ -27,7 +27,7 @@ class IEMOCAP(Dataset):
 
     def __getitem__(self, index):
         input1 = torch.squeeze(F.interpolate(torch.unsqueeze(torch.from_numpy(10*np.log10(self.input1[index])).float(), dim=0), size=230, mode='nearest'), dim=0)
-        input2 = torch.squeeze(F.interpolate(torch.unsqueeze(torch.from_numpy(10*np.log10(self.input2[index])).float(), dim=0), size=120, mode='nearest'), dim=0)
+        input2 = torch.squeeze(F.interpolate(torch.unsqueeze(torch.from_numpy(10*np.log10(self.input2[index])).float(), dim=0), size=230, mode='nearest'), dim=0)
         sample = {'input_lstm': torch.from_numpy(self.input_lstm[index]).float(),
                   'seq_length': self.input_lstm[index].shape[0],
                   'input1': input1,
