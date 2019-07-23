869465c11d1e47dd8de47cbc9e34c438268149a5
wjyandre <wangjianyou100044@gmail.com>
2019-07-23 15:38:04 -0400

    changes

--- src/speech/model_FT_LSTM.py
+++ src/speech/model_FT_LSTM.py
@@ -248,7 +248,7 @@ class CNN_FTLSTM(nn.Module):
         self.special=special
         if self.special=="concat":
             self.classification_raw=nn.Linear(hidden_dim*2,self.num_labels).to(self.device)
-        elif self.special=="attention"
+        elif self.special=="attention":
             self.classification_raw=nn.Linear(hidden_dim*2,self.num_labels).to(self.device)
             self.attention=nn.Sequential(nn.Linear(hidden_dim*2,hidden_dim),
                                         nn.sigmoid()).to(self.device)
