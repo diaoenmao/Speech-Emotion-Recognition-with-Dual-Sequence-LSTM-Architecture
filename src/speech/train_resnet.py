a39a54340da72441f9a6a339a3bb3470c17047a2
Michael Xue <michael.xue@duke.edu>
2019-07-25 15:01:54 -0400

    changes

--- src/speech/train_resnet.py
+++ src/speech/train_resnet.py
@@ -93,7 +93,7 @@ def train_model(args):
                 print("=================================Train Batch"+ str(j+1)+str(weight)+"===================================================")
             model.zero_grad()
             x = model(input1)
-            target_index = torch.argmax(target, dim=1).to(self.device)
+            target_index = torch.argmax(target, dim=1)
             correct_batch=torch.sum(target_index==torch.argmax(x,dim=1))
             losses_batch=F.cross_entropy(x,torch.max(target,1)[1])
             correct_batch=torch.unsqueeze(correct_batch,dim=0)
