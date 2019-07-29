c9cbc813ac3adf1f16174b36c97deb1780457388
wjyandre <wangjianyou100044@gmail.com>
2019-07-29 18:50:04 -0400

    changes

--- src/speech/gpu_FT_LSTM4.sh
+++ src/speech/gpu_FT_LSTM4.sh
@@ -1,5 +1,4 @@
-python train_FT_LSTM3.py -b 128 -out1 16 -out2 64 -kc1 4 -kc2 4 -sc1 1 -sc2 1 -kp1 2 -kp2 2 -sp 2 -w 0.6 -special "attention" -f "experiment3"
-python train_FT_LSTM3.py -b 128 -out1 32 -out2 64 -kc1 4 -kc2 2 -sc1 1 -sc2 1 -kp1 2 -kp2 2 -sp 2 -w 0.5 -special "attention" -f "experiment3"
+#!/bin/bash
 python train_FT_LSTM3.py -b 128 -out1 32 -out2 64 -kc1 5 -kc2 3 -sc1 1 -sc2 1 -kp1 2 -kp2 2 -sp 2 -w 0.5 -special "attention" -f "experiment3"
 python train_FT_LSTM3.py -b 128 -out1 64 -out2 32 -kc1 5 -kc2 3 -sc1 1 -sc2 1 -kp1 2 -kp2 2 -sp 2 -w 0.6 -special "attention" -f "experiment3"
 python train_FT_LSTM3.py -b 128 -out1 64 -out2 32 -kc1 4 -kc2 4 -sc1 1 -sc2 1 -kp1 2 -kp2 2 -sp 2 -w 0.5 -special "attention" -f "experiment3"
