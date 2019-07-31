0406c78ca0465f8ddef3163bfab67f0f958de384
wjyandre <wangjianyou100044@gmail.com>
2019-07-31 14:34:14 -0400

    changes

--- src/final_model/gpu.sh
+++ src/final_model/gpu.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python train_final.py -b 128 -f "experiment7"
-python train_final2.py -b 128 -f "experiment7"
-python train_final.py -b 128 -f "experiment7"
-python train_final2.py -b 128 -f "experiment7"
+python train_final.py -b 128 -f "experiment8"
+python train_final2.py -b 128 -f "experiment8"
+python train_final.py -b 128 -f "experiment8"
+python train_final2.py -b 128 -f "experiment8"
