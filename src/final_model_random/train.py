8bd2f6548ce046c6310fa7c72d940ac1218f621d
wjyandre <wangjianyou100044@gmail.com>
2019-08-01 17:00:14 -0400

    changes

--- src/final_model_random/train.py
+++ src/final_model_random/train.py
@@ -39,6 +39,7 @@ def train_model(args):
     if args.model=="model7": from model7 import CNN_FTLSTM
     if args.model=="base1": from base1 import CNN_FTLSTM
     if args.model=="base2": from base2 import CNN_FTLSTM
+    if args.model=="base3": from base3 import CNN_FTLSTM
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     if torch.cuda.is_available():torch.cuda.manual_seed_all(999)
     np.random.seed(999)
