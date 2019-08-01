0f53da8bb8892d59305d8aec856d2b585b52654b
wjyandre <wangjianyou100044@gmail.com>
2019-08-01 10:46:53 -0400

    changes

--- src/final_model_loso/train_loso.py
+++ src/final_model_loso/train_loso.py
@@ -64,7 +64,7 @@ def train_model(args):
         print("============================ Number of parameters ====================================")
         print(str(sum(p.numel() for p in model.parameters() if p.requires_grad)))
         training_data = IEMOCAP(session=session, train=True)
-        train_loader = DataLoader(dataset=training_data, batch_size=batch_size, shuffle=True, collate_fn=my_collate, num_workers=0, drop_last=True)
+        train_loader = DataLoader(dataset=training_data, batch_size=batch_size, shuffle=True, collate_fn=my_collate, num_workers=0, drop_last=False)
         testing_data = IEMOCAP(session=session, train=False)
         test_loader = DataLoader(dataset=testing_data, batch_size=batch_size, shuffle=True, collate_fn=my_collate, num_workers=0,drop_last=False)
 
