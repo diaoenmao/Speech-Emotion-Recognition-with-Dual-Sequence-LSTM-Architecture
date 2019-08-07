ee42e33aca38d9a271079e423dff8452d9af54c9
wjyandre <wangjianyou100044@gmail.com>
2019-08-06 20:56:22 -0400

    changes

--- src/final_model_random/model10.py
+++ src/final_model_random/model10.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import pdb
 import numpy as np
+# model 4 without Audio_lstm
 class SeparatedBatchNorm1d(nn.Module):
     def __init__(self, num_features, max_length, eps=1e-5, momentum=0.1):
         super(SeparatedBatchNorm1d, self).__init__()
@@ -35,21 +36,7 @@ class SeparatedBatchNorm1d(nn.Module):
             input=input_, running_mean=running_mean, running_var=running_var,
             weight=self.weight, bias=self.bias, training=self.training,
             momentum=self.momentum, eps=self.eps)
-class LSTM_Audio(nn.Module):
-    def __init__(self, hidden_dim,num_layers,device,dropout_rate=0 ,bidirectional=False):
-        super(LSTM_Audio, self).__init__()
-        self.device = device
-        self.num_features = 39
-        self.hidden_dim = hidden_dim
-        self.num_layers = num_layers
-        self.dropout_rate = dropout_rate
-        self.bidirectional = bidirectional
-        self.lstm = nn.LSTM(self.num_features, self.hidden_dim, self.num_layers, batch_first=True,
-                           dropout=self.dropout_rate, bidirectional=self.bidirectional).to(self.device)
-    def forward(self, input_lstm):
-        input_lstm = input_lstm.to(self.device)
-        out_lstm,_ = self.lstm(input_lstm)
-        return out_lstm
+
 class LFLB(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size_cnn, stride_cnn, padding_cnn, padding_pool,kernel_size_pool, stride_pool, device):
         super(LFLB, self).__init__()
@@ -86,6 +73,7 @@ class FTLSTMCell(nn.Module):
         self.inputy_dim=inputy_dim
         # BN parameters
         self.batch = SeparatedBatchNorm1d(num_features=4 * self.hidden_dim, max_length=max_length)
+        self.batchhT = nn.BatchNorm1d(num_features=self.hidden_dim)
 
         self.W=nn.Linear(self.inputx_dim+self.inputy_dim+self.hidden_dim,4*self.hidden_dim,bias=True)
         self.WTc=nn.Linear(self.inputx_dim+self.hidden_dim,self.hidden_dim,bias=True)
@@ -105,7 +93,7 @@ class FTLSTMCell(nn.Module):
         C_F=torch.tanh(self.WFc(torch.cat([y,hT],dim=1)))
         CT=fT*CT+iT*C_T+iF*C_F
         hT=oT*torch.tanh(CT)
-        outT=hT
+        outT=self.batchhT(hT)
         return outT,hT,CT
     def init_hidden(self, batch_size):
         return (nn.Parameter(torch.zeros(batch_size, self.hidden_dim)).to(self.device),
@@ -260,8 +248,6 @@ class CNN_FTLSTM(nn.Module):
         cell=FTLSTM(time,inputx_dim,inputy_dim,hidden_dim,num_layers_ftlstm,device)
         setattr(self,"ftlstm",cell)
         self.weight=nn.Parameter(torch.FloatTensor([weight]),requires_grad=False)
-        self.LSTM_Audio=LSTM_Audio(self.hidden_dim_lstm,self.num_layers,self.device)
-        self.classification_hand = nn.Linear(self.hidden_dim_lstm, self.num_labels).to(self.device)
         self.classification_raw=nn.Linear(hidden_dim,self.num_labels).to(self.device)
 
     def forward(self,input_lstm,input1,input2,target,seq_length,train=True):
@@ -274,20 +260,14 @@ class CNN_FTLSTM(nn.Module):
         inputx,inputy=getattr(self,"cnn_multi")(input1,input2)
         outT=getattr(self,"ftlstm")(inputx,inputy)
 
-        out_lstm = self.LSTM_Audio(input_lstm).permute(0,2,1)
-        temp = [torch.unsqueeze(torch.mean(out_lstm[k,:,:int(s.item())],dim=1),dim=0) for k,s in enumerate(seq_length)]
-        out_lstm = torch.cat(temp,dim=0)
         out=torch.mean(outT,dim=2)
         out = self.classification_raw(out)
-        out_lstm = self.classification_hand(out_lstm)
         p = self.weight
-        out_final = p*out+(1-p)*out_lstm
+        out_final = out
         target_index = torch.argmax(target, dim=1).to(self.device)
         pred_index = torch.argmax(out_final, dim=1).to(self.device)
         correct_batch=torch.sum(target_index==torch.argmax(out_final,dim=1))
-        losses_batch_hand=F.cross_entropy(out_lstm,torch.max(target,1)[1])
-        losses_batch_raw=F.cross_entropy(out,torch.max(target,1)[1])
-        losses_batch=p*losses_batch_raw+(1-p)*losses_batch_hand
+        losses_batch=F.cross_entropy(out_final,torch.max(target,1)[1])
         correct_batch=torch.unsqueeze(correct_batch,dim=0)
         losses_batch=torch.unsqueeze(losses_batch, dim=0)
         if train:
