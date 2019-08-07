c340f09fbddfaa60f78e746d8c893935b752e0f8
wjyandre <wangjianyou100044@gmail.com>
2019-08-07 14:04:28 -0400

    changes

--- src/final_model_random/train.py
+++ src/final_model_random/train.py
@@ -48,7 +48,7 @@ def train_model(args):
     if args.model=="base5": from base5 import CNN_FTLSTM
     if args.model=="base6": from base5 import CNN_FTLSTM
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    experiment=args.experiment(10)
+    experiment=args.experiment
     device_ids=[0,1,2,3]
     num_devices=len(device_ids)
     batch_size=args.batch_size
