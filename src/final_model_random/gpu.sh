082693a57e1e52296301986127922840f843451c
wjyandre <wangjianyou100044@gmail.com>
2019-08-10 17:53:00 -0400

    changes

--- src/final_model_random/gpu.sh
+++ src/final_model_random/gpu.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-python train.py -m "model4" -kc1 5 -kc2 5 -f "new_experiment_model4"  -e 10
-python train.py -m "model4" -kc1 4 -kc2 4 -f "new_experiment_model4" -e 10
-python train.py -m "model4" -kc1 3 -kc2 3 -f "new_experiment_model4" -e 10
-python train.py -out1 64 -out2 32 -kc1 5 -kc2 5 -w 0.5 -f "new_experiment_model4" -m "model4" -e 10
 python train.py -out1 64 -out2 32 -kc1 4 -kc2 4 -w 0.5 -f "new_experiment_model4" -m "model4" -e 10
 python train.py -out1 64 -out2 32 -kc1 3 -kc2 3 -w 0.5 -f "new_experiment_model4" -m "model4" -e 10
 python train.py -out1 128 -out2 16 -kc1 5 -kc2 5 -w 0.5 -f "new_experiment_model4" -m "model4" -e 10
--- src/final_model_random/gpu2.sh
+++ src/final_model_random/gpu2.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-python train.py -m "model10" -f "experiment_model10"
-python train.py -out1 64 -out2 32 -kc1 5 -kc2 3 -w 0.55 -f "experiment_model10" -m "model10"
-python train.py -out1 16 -out2 64 -kc1 4 -kc2 4 -w 0.6 -f "experiment_model10" -m "model10"
-python train.py -out1 32 -out2 64 -kc1 4 -kc2 2 -w 0.5 -f "experiment_model10" -m "model10"
-python train.py -out1 32 -out2 64 -kc1 5 -kc2 3 -w 0.5 -f "experiment_model10" -m "model10"
-python train.py -out1 64 -out2 32 -kc1 5 -kc2 3 -w 0.6 -f "experiment_model10" -m "model10"
-python train.py -out1 64 -out2 32 -kc1 4 -kc2 4 -w 0.5 -f "experiment_model10" -m "model10"
-python train.py -out1 128 -out2 16 -kc1 3 -kc2 3 -w 0.5 -f "experiment_model10" -m "model10"
+python train.py -m "model10" -kc1 5 -kc2 5 -f "new_experiment_model10"  -e 10
