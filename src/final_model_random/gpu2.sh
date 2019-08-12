622f2fa960ccd064ff08b9450ee54763f3e1e0d7
wjyandre <wangjianyou100044@gmail.com>
2019-08-11 21:40:09 -0400

    changes

--- src/final_model_random/gpu2.sh
+++ src/final_model_random/gpu2.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python train.py -b1 129 -m "model10" -kc1 5 -kc2 5 -f "new_experiment_model10"  -e 10
+python train.py -b 129 -m "model10" -kc1 5 -kc2 5 -f "new_experiment_model10"  -e 10
